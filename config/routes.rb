5502b7ea68f4711ff4a37189f0e6a3dfe4a3b137
Eric Lee Treece <elt106@traject-test.vmhost.psu.edu>
2018-01-17 16:24:09 -0500

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,28 @@
+Rails.application.routes.draw do
+  
+  mount Blacklight::Engine => '/'
+  Blacklight::Marc.add_routes(self)
+  root to: "catalog#index"
+    concern :searchable, Blacklight::Routes::Searchable.new
+
+  resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
+    concerns :searchable
+  end
+
+  devise_for :users
+  concern :exportable, Blacklight::Routes::Exportable.new
+
+  resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
+    concerns :exportable
+  end
+
+  resources :bookmarks do
+    concerns :exportable
+
+    collection do
+      delete 'clear'
+    end
+  end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
