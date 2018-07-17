cb27e243fdebe4bbb75bcb7af16b4a328e1d6bc0
Charlie Morris <cdm32@psu.edu>
2018-07-17 09:48:12 -0400

    working state, needed to add popper.js https://github.com/twbs/bootstrap/issues/23381#issuecomment-322179061
    must stop webpacker dev server and the rails server, run yarn install, then turn on the webpacker dev server

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,9 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
+require('popper.js/dist/umd/popper');
+require('bootstrap/dist/js/bootstrap');
 require('blacklight-frontend/app/assets/javascripts/blacklight/blacklight')
+
 
 console.log('Hello World from Webpacker')
