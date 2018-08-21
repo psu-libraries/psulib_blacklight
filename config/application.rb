de367f07ff9a7c725c33835ee29d241a7ce29b07
Charlie Morris <cdm32@psu.edu>
2018-08-21 14:58:35 -0400

    see https://stackoverflow.com/a/41560187/721065, Traject was an uninitialized constant on deploy

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module Psulcat
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.enable_dependency_loading = true
+    config.autoload_paths << Rails.root.join('lib')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
