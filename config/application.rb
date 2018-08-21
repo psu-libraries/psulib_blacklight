e88d8d13524458b1df53f053539978c7d66422a3
Charlie Morris <cdm32@psu.edu>
2018-08-21 15:04:18 -0400

    nope

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Psulcat
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 5.1
     config.enable_dependency_loading = true
     config.autoload_paths << Rails.root.join('lib')
     # Settings in config/environments/* take precedence over those specified here.
