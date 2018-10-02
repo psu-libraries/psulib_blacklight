f42400be3c6c3f90a82464cd80e14ddc38b1cb67
Charlie Morris <cdm32@psu.edu>
2018-10-02 13:02:05 -0400

    formatting

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -1,21 +1,19 @@
 # frozen_string_literal: true
 class SolrDocument
-  include Blacklight::Solr::Document    
-      # The following shows how to setup this blacklight document to display marc documents
+  include Blacklight::Solr::Document
+  # The following shows how to setup this blacklight document to display marc documents
   extension_parameters[:marc_source_field] = :marc_display
   extension_parameters[:marc_format_type] = :marcxml
-  use_extension( Blacklight::Solr::Document::Marc) do |document|
-    document.key?( :marc_display  )
+  use_extension(Blacklight::Solr::Document::Marc) do |document|
+    document.key?(:marc_display)
   end
-  
-  field_semantics.merge!(    
-                         :title => "title_display",
-                         :author => "author_display",
-                         :language => "language_facet",
-                         :format => "format"
-                         )
 
-
+  field_semantics.merge!(
+      :title => "title_display",
+      :author => "author_display",
+      :language => "language_facet",
+      :format => "format"
+  )
 
   # self.unique_key = 'id'
 
