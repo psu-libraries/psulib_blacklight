2abed95acb4f8ee3e9a70e9abafa4b6ab1c3695e
Charlie Morris <cdm32@psu.edu>
2018-11-13 11:19:02 -0500

    pub_date_sort conversion

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -207,10 +207,10 @@ class CatalogController < ApplicationController
     # label in pulldown is followed by the name of the SOLR field to sort by and
     # whether the sort is ascending or descending (it must be asc or desc
     # except in the relevancy case).
-    config.add_sort_field 'score desc, pub_date_sort desc, title_ssort asc', label: 'relevance'
-    config.add_sort_field 'pub_date_sort desc, title_ssort asc', label: 'year'
+    config.add_sort_field 'score desc, pub_date_sort_itsi desc, title_ssort asc', label: 'relevance'
+    config.add_sort_field 'pub_date_sort_itsi desc, title_ssort asc', label: 'year'
     config.add_sort_field 'author_ssort asc, title_ssort asc', label: 'author'
-    config.add_sort_field 'title_ssort asc, pub_date_sort desc', label: 'title'
+    config.add_sort_field 'title_ssort asc, pub_date_sort_itsi desc', label: 'title'
 
     # If there are more than this many search results, no spelling ("did you
     # mean") suggestion is offered.
