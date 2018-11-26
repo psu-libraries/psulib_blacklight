6ff557c2ff82587742a2bb6229ad4a68e53ad40c
Charlie Morris <cdm32@psu.edu>
2018-11-26 09:53:17 -0500

    Reformatting the pending tests so that they appear pending and they don't fail.

--- spec/features/record_page_fields_spec.rb
+++ spec/features/record_page_fields_spec.rb
@@ -4,24 +4,19 @@ require 'rails_helper'
 
 RSpec.describe 'RecordPageFields', type: :feature do
   describe 'Title fields' do
-    it 'User visits a document with a vernacular title' do
-      pending('Vernacular title field will be displayed as title, title will appear below with label title')
-    end
+    it 'User visits a document with a vernacular title'
+    # Vernacular title field will be displayed as title, title will appear below with label title')
 
-    it 'User visits a document without a vernacular title' do
-      pending('Title field will be displayed as title')
-    end
+    it 'User visits a document without a vernacular title'
+    # Title field will be displayed as title
 
-    it 'User visits a document with a uniform title' do
-      pending('Uniform title field will be displayed')
-    end
+    it 'User visits a document with a uniform title'
+    # Uniform title field will be displayed
 
-    it 'User visits a document with additional titles' do
-      pending('Additional title field will be displayed')
-    end
+    it 'User visits a document with additional titles'
+    # Additional title field will be displayed
 
-    it 'User visits a document with a series title' do
-      pending('Series title field will be displayed')
-    end
+    it 'User visits a document with a series title'
+    # Series title field will be displayed
   end
 end
