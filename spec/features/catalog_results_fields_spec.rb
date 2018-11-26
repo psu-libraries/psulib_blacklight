a769676d611e03897595e197e2adc78dd2e0011f
Charlie Morris <cdm32@psu.edu>
2018-11-26 09:29:37 -0500

    Fixes made by `rubocop -a`.

--- spec/features/catalog_results_fields_spec.rb
+++ spec/features/catalog_results_fields_spec.rb
@@ -1,28 +1,28 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.feature "CatalogResultsFields", type: :feature do
-
-  feature "Title fields" do
-    scenario "User does a search that returns at least one result without a vernacular title" do
+RSpec.describe 'CatalogResultsFields', type: :feature do
+  describe 'Title fields' do
+    it 'User does a search that returns at least one result without a vernacular title' do
       # will have a sub-field titled "Title:" with the original, vernacular will be in the actual lead statment
     end
 
-    scenario "User does a search that returns at least one result with a vernacular title" do
+    it 'User does a search that returns at least one result with a vernacular title' do
       # no Title: subfield
       # some way of identifying the vernacular is indeed the title?
     end
 
-    scenario "User does a search that returns one result with a uniform title" do
+    it 'User does a search that returns one result with a uniform title' do
       # will expect this field to not be present
     end
 
-    scenario "User does a search that returns one result with an additional title" do
+    it 'User does a search that returns one result with an additional title' do
       # will expect this field to not be present
     end
 
-    scenario "User does a search that returns one result with a series title" do
+    it 'User does a search that returns one result with a series title' do
       # will expect this field to not be present
     end
   end
-
 end
--- spec/features/record_page_fields_spec.rb
+++ spec/features/record_page_fields_spec.rb
@@ -1,28 +1,22 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.feature "RecordPageFields", type: :feature do
-  feature "Title fields" do
-
-    scenario "User visits a document with a vernacular title" do
-
+RSpec.describe 'RecordPageFields', type: :feature do
+  describe 'Title fields' do
+    it 'User visits a document with a vernacular title' do
     end
 
-    scenario "User visits a document without a vernacular title" do
-
+    it 'User visits a document without a vernacular title' do
     end
 
-    scenario "User visits a document without a uniform title" do
-
+    it 'User visits a document without a uniform title' do
     end
 
-    scenario "User visits a document without additional titles" do
-
+    it 'User visits a document without additional titles' do
     end
 
-    scenario "User visits a document without a series title" do
-
+    it 'User visits a document without a series title' do
     end
-
   end
-
 end
