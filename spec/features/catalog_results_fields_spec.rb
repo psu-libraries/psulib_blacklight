eac555987bbae1329d5234f26db7388077b7e4f9
Charlie Morris <cdm32@psu.edu>
2018-11-26 09:41:12 -0500

    Clearing up other rubocop offenses by using `pending()` instead of a comment line.

--- spec/features/catalog_results_fields_spec.rb
+++ spec/features/catalog_results_fields_spec.rb
@@ -5,24 +5,25 @@ require 'rails_helper'
 RSpec.describe 'CatalogResultsFields', type: :feature do
   describe 'Title fields' do
     it 'User does a search that returns at least one result without a vernacular title' do
-      # will have a sub-field titled "Title:" with the original, vernacular will be in the actual lead statment
+      pending('will have a sub-field titled \"Title:\" with the original, vernacular will be in the actual lead
+              statement')
     end
 
     it 'User does a search that returns at least one result with a vernacular title' do
       # no Title: subfield
-      # some way of identifying the vernacular is indeed the title?
+      pending('some way of identifying the vernacular is indeed the title?')
     end
 
     it 'User does a search that returns one result with a uniform title' do
-      # will expect this field to not be present
+      pending('will expect uniform title field to not be present')
     end
 
     it 'User does a search that returns one result with an additional title' do
-      # will expect this field to not be present
+      pending('will expect additional field to not be present')
     end
 
     it 'User does a search that returns one result with a series title' do
-      # will expect this field to not be present
+      pending('will expect series title field to not be present')
     end
   end
 end
--- spec/features/record_page_fields_spec.rb
+++ spec/features/record_page_fields_spec.rb
@@ -5,18 +5,23 @@ require 'rails_helper'
 RSpec.describe 'RecordPageFields', type: :feature do
   describe 'Title fields' do
     it 'User visits a document with a vernacular title' do
+      pending('Vernacular title field will be displayed as title, title will appear below with label title')
     end
 
     it 'User visits a document without a vernacular title' do
+      pending('Title field will be displayed as title')
     end
 
-    it 'User visits a document without a uniform title' do
+    it 'User visits a document with a uniform title' do
+      pending('Uniform title field will be displayed')
     end
 
-    it 'User visits a document without additional titles' do
+    it 'User visits a document with additional titles' do
+      pending('Additional title field will be displayed')
     end
 
-    it 'User visits a document without a series title' do
+    it 'User visits a document with a series title' do
+      pending('Series title field will be displayed')
     end
   end
 end
