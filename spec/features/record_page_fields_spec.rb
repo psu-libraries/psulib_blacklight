7b956cad90381587447d5548e84dad8afc2d8801
Charlie Morris <cdm32@psu.edu>
2018-11-27 16:25:55 -0500

    Removed label expectations and added another feature for Related Title.

--- spec/features/record_page_fields_spec.rb
+++ spec/features/record_page_fields_spec.rb
@@ -17,7 +17,6 @@ RSpec.describe 'RecordPageFields', type: :feature do
       visit "/catalog/#{doc_vern['id']}"
       expect(page).to have_selector 'h1', count: 1
       expect(page).to have_selector 'h1', text: (doc_vern['title_display_ssm'][0]).to_s
-      expect(page).to have_selector 'dt.blacklight-title_latin_display_ssm', text: 'Title:', count: 1
       expect(page).to have_selector 'dd.blacklight-title_latin_display_ssm', count: 1
       expect(page).to have_selector 'dd.blacklight-title_latin_display_ssm',
                                     text: (doc_vern['title_latin_display_ssm'][0]).to_s, count: 1
@@ -49,8 +48,6 @@ RSpec.describe 'RecordPageFields', type: :feature do
     scenario 'User visits a document with additional titles' do
       # Additional title field will be displayed
       visit "/catalog/#{doc_addl['id']}"
-      expect(page).to have_selector 'dt.blacklight-additional_title_display_ssm',
-                                    text: 'Additional Titles:', count: 1
       expect(page).to have_selector 'dd.blacklight-additional_title_display_ssm', count: 1
       expect(page).to have_selector 'dd.blacklight-additional_title_display_ssm',
                                     text: (doc_addl['additional_title_display_ssm'][0]).to_s, count: 1
@@ -63,11 +60,21 @@ RSpec.describe 'RecordPageFields', type: :feature do
     scenario 'User visits a document with a series title' do
       # Series title field will be displayed
       visit "/catalog/#{doc_series['id']}"
-      expect(page).to have_selector 'dt.blacklight-series_title_display_ssm',
-                                    text: 'Series:', count: 1
       expect(page).to have_selector 'dd.blacklight-series_title_display_ssm', count: 1
       expect(page).to have_selector 'dd.blacklight-series_title_display_ssm',
                                     text: (doc_series['series_title_display_ssm'][0]).to_s, count: 1
+    end
+  end
+
+  feature 'Series title' do
+    let (:doc_related) { documents.find { |i| i['id'] == '13217672' } }
+
+    scenario 'User visits a document with a related title' do
+      # Series title field will be displayed
+      visit "/catalog/#{doc_related['id']}"
+      expect(page).to have_selector 'dd.blacklight-related_title_display_ssm', count: 1
+      expect(page).to have_selector 'dd.blacklight-related_title_display_ssm',
+                                    text: (doc_related['related_title_display_ssm'][0]).to_s, count: 1
     end
   end
 end
