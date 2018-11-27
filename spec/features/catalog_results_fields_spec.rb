ad84864103e28bfe4872e92a8a88d8ffd816520a
Charlie Morris <cdm32@psu.edu>
2018-11-27 14:54:04 -0500

    Title specs added for item level
    * Switched to using our JSON solr fixture data to supply expected values
    * Separated scenarios into features

--- spec/features/catalog_results_fields_spec.rb
+++ spec/features/catalog_results_fields_spec.rb
@@ -4,8 +4,9 @@ require 'rails_helper'
 
 RSpec.describe 'CatalogResultsFields', type: :feature do
   describe 'Title fields' do
-    it 'User does a search that returns at least one result without a vernacular title'
-    # will have a sub-field titled \"Title:\" with the original, vernacular will be in the actual lead statement
+    it 'User does a search that returns at least one result without a vernacular title' do
+      # will have a sub-field titled \"Title:\" with the original, vernacular will be in the actual lead statement
+    end
 
     it 'User does a search that returns at least one result with a vernacular title'
     # no Title: subfield
--- spec/features/record_page_fields_spec.rb
+++ spec/features/record_page_fields_spec.rb
@@ -3,62 +3,71 @@
 require 'rails_helper'
 
 RSpec.describe 'RecordPageFields', type: :feature do
-  describe 'Title fields' do
+  let (:documents) {
+    fixtures_doc = File.read("#{fixture_path}/current_fixtures.json")
+    JSON.parse(fixtures_doc)
+  }
+
+  feature 'Vernacular titles' do
+    let (:doc_vern) { documents.find { |i| i['id'] == '2788022' } }
+    let (:doc_no_vern) { documents.find { |i| i['id'] == '2431513' } }
+
     scenario 'User visits a document with a vernacular title' do
-      pending 'Run these once the vernacular title feature is being worked on'
       # Vernacular title field will be displayed as title, title will appear below with label title')
-      # TODO: pluralize label
-      visit '/catalog/2788022'
+      visit "/catalog/#{doc_vern['id']}"
       expect(page).to have_selector 'h1', count: 1
-      expect(page).to have_selector 'h1', text: '小說ワンダフルライフ'
-      expect(page).to have_selector 'dt.blacklight-title_display_ssm', text: 'Title:', count: 1
-      expect(page).to have_selector 'dd.blacklight-title_display_ssm', count: 1
-      expect(page).to have_selector 'dd.blacklight-title_display_ssm',
-                                    text: 'Shōsetsu wandafuru raifu', count: 1
+      expect(page).to have_selector 'h1', text: (doc_vern['title_display_ssm'][0]).to_s
+      expect(page).to have_selector 'dt.blacklight-title_latin_display_ssm', text: 'Title:', count: 1
+      expect(page).to have_selector 'dd.blacklight-title_latin_display_ssm', count: 1
+      expect(page).to have_selector 'dd.blacklight-title_latin_display_ssm',
+                                    text: (doc_vern['title_latin_display_ssm'][0]).to_s, count: 1
     end
 
     scenario 'User visits a document without a vernacular title' do
       # Title field will be displayed as title
-      visit '/catalog/2431513'
+      visit "/catalog/#{doc_no_vern['id']}"
       expect(page).to have_selector 'h1', count: 1
-      expect(page).to have_selector 'h1', text: 'La ressemblance'
+      expect(page).to have_selector 'h1', text: (doc_no_vern['title_display_ssm'][0]).to_s
     end
+  end
+
+  feature 'Uniform title' do
+    let (:doc_uni) { documents.find { |i| i['id'] == '286971' } }
 
     scenario 'User visits a document with a uniform title' do
       # Uniform title field will be displayed
-      # TODO: pluralize label
-      visit '/catalog/286971'
-      expect(page).to have_selector 'dt.blacklight-uniform_title_display_ssm',
-                                    text: 'Uniform Title:', count: 1
+      visit "/catalog/#{doc_uni['id']}"
       expect(page).to have_selector 'dd.blacklight-uniform_title_display_ssm', count: 1
       expect(page).to have_selector 'dd.blacklight-uniform_title_display_ssm',
-                                    text: 'Adamo ed Eva', count: 1
+                                    text: (doc_uni['uniform_title_display_ssm'][0]).to_s, count: 1
     end
+  end
+
+  feature 'Additional titles' do
+    let (:doc_addl) { documents.find { |i| i['id'] == '21835545' } }
 
     scenario 'User visits a document with additional titles' do
-      pending 'Run these once the additional title feature is being worked on'
       # Additional title field will be displayed
-      # TODO: pluralize label
-      additional_title_example = 'Fleurs du mal (Les) / Hungarian Fantasy / In the Jungle (Tabery, Redik, Recreation -'\
-                                 ' Graz Grosses Orchester, Swierczewski)'
-      visit '/catalog/21835545'
+      visit "/catalog/#{doc_addl['id']}"
       expect(page).to have_selector 'dt.blacklight-additional_title_display_ssm',
-                                    text: 'Additional Title:', count: 1
+                                    text: 'Additional Titles:', count: 1
       expect(page).to have_selector 'dd.blacklight-additional_title_display_ssm', count: 1
       expect(page).to have_selector 'dd.blacklight-additional_title_display_ssm',
-                                    text: additional_title_example, count: 1
+                                    text: (doc_addl['additional_title_display_ssm'][0]).to_s, count: 1
     end
+  end
+
+  feature 'Series title' do
+    let (:doc_series) { documents.find { |i| i['id'] == '1618709' } }
 
     scenario 'User visits a document with a series title' do
-      pending 'Run these once the series title feature is being worked on'
       # Series title field will be displayed
-      # TODO: pluralize label
-      visit '/catalog/1618709'
+      visit "/catalog/#{doc_series['id']}"
       expect(page).to have_selector 'dt.blacklight-series_title_display_ssm',
-                                    text: 'Series Title:', count: 1
+                                    text: 'Series:', count: 1
       expect(page).to have_selector 'dd.blacklight-series_title_display_ssm', count: 1
       expect(page).to have_selector 'dd.blacklight-series_title_display_ssm',
-                                    text: 'Acappella artists', count: 1
+                                    text: (doc_series['series_title_display_ssm'][0]).to_s, count: 1
     end
   end
 end
