6268df5869fd3a4c1a127d0485906c2861ceebc0
Charlie Morris <cdm32@psu.edu>
2018-12-10 13:44:33 -0500

    Adds a new field called bound_with_title_struct along with a new helper to make a link to a bound item's parent

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  # Makes a link to the item's bound in title. Could be refactored later to be more generic.
+  def link_it options={}
+    options[:value] # the value of the field
+    field_data = options[:value].first
+    field_data = JSON.parse field_data
+
+    link_to field_data['boundtitle'], "/catalog/#{field_data['boundcatkey']}"
+  end
 end
