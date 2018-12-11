7061c4e009b2fb1315078e81b130144be2e87676
Charlie Morris <cdm32@psu.edu>
2018-12-11 15:19:51 -0500

    making the link_it method more generalized

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@
 
 module ApplicationHelper
   # Makes a link to the item's bound in title. Could be refactored later to be more generic.
-  def link_it(options = {})
+  def catalog_link(options = {})
     options[:value] # the value of the field
     field_data = options[:value].first
     field_data = JSON.parse field_data
 
-    link_to field_data['boundtitle'], "/catalog/#{field_data['boundcatkey']}"
+    link_to field_data['linktext'], "/catalog/#{field_data['catkey']}"
   end
 end
