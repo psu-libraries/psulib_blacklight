9bdace00200f809ed4152c6c88d444b588ff6bc7
Charlie Morris <cdm32@psu.edu>
2018-12-12 13:56:51 -0500

    rubocop'd

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
   describe '#catalog_link' do
-    let (:catalog_link_field_data) { ["{\"catkey\": \"355035\", \"linktext\": \"The high-caste Hindu woman / With introduction by Rachel L. Bodley\"}"] }
-    let (:catalog_link_doc) { {value: catalog_link_field_data } }
+    let (:catalog_link_field_data) { ['{"catkey": "355035", "linktext": "The high-caste Hindu woman / With introduction by Rachel L. Bodley"}'] }
+    let (:catalog_link_doc) { { value: catalog_link_field_data } }
 
     context 'has a single value for a link field' do
       it 'assembles a link' do
