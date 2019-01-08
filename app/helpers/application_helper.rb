856c0785429ac58bf30947ceeb99e614261d5eaa
Banu Kutlu <bzk60@psu.edu>
2019-01-08 11:35:19 -0500

    subjects display with hierarchical linking, also adding _show_default partial for displaying lists, as part of #3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,36 @@ module ApplicationHelper
 
     link_to field_data['linktext'], "/catalog/#{field_data['catkey']}"
   end
+
+  SEPARATOR = '—'
+  QUERYSEP = '—'
+  def subjectify(args)
+    all_subjects = []
+    sub_array = []
+    args[:document][args[:field]].each_with_index do |subject, i|
+      spl_sub = subject.split(QUERYSEP)
+      sub_array << []
+      subject_accum = ''
+      spl_sub.each_with_index do |subsubject, j|
+        spl_sub[j] = subject_accum + subsubject
+        subject_accum = spl_sub[j] + QUERYSEP
+        sub_array[i] << spl_sub[j]
+      end
+      all_subjects[i] = subject.split(QUERYSEP)
+    end
+    args[:document][args[:field]].each_with_index do |_subject, i|
+      lnk = ''
+      lnk_accum = ''
+      all_subjects[i].each_with_index do |subsubject, j|
+        lnk = lnk_accum + link_to(subsubject,
+                                  "/?f[subject_facet][]=#{CGI.escape sub_array[i][j]}",
+                                  class: 'search-subject',
+                                  'data-toggle' => 'tooltip',
+                                  'data-original-title' => "Search: #{sub_array[i][j]}",
+                                  title: "Search: #{sub_array[i][j]}")
+        lnk_accum = lnk + content_tag(:span, SEPARATOR) # , class: 'subject-level'
+      end
+      args[:document][args[:field]][i] = lnk.html_safe
+    end
+  end
 end
