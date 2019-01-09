530a52dd16ea56048eb9bc2a50901f6f10acdf9a
Charlie Morris <cdm32@psu.edu>
2019-01-09 09:49:10 -0500

    incidental refactor on this branch, unnecessary line

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,6 @@
 module ApplicationHelper
   # Makes a link to a catalog item.
   def catalog_link(options = {})
-    options[:value] # the value of the field
     field_data = options[:value].first
     field_data = JSON.parse field_data
 
