2b56a9c25ab303e1afd47d477a1bb57f66a1b6a3
Charlie Morris <523381+cdmo@users.noreply.github.com>
2019-01-30 15:39:04 -0500

    Changing to "Keyword" to be up to the requirements

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -191,7 +191,7 @@ class CatalogController < ApplicationController
     # solr request handler? The one set in config[:default_solr_parameters][:qt],
     # since we aren't specifying it otherwise.
 
-    config.add_search_field 'all_fields', label: 'All Fields'
+    config.add_search_field 'all_fields', label: 'Keyword'
 
     # Now we see how to over-ride Solr request handler defaults, in this
     # case for a BL "search field", which is really a dismax aggregate
