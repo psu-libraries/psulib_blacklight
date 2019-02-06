fec84f920eb83ab0ff3a9aabe15e4fd7492cf181
Banu Kutlu <bzk60@psu.edu>
2019-02-06 10:05:16 -0500

    should have override fields method

--- app/presenters/psul_index_presenter.rb
+++ app/presenters/psul_index_presenter.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class PsulIndexPresenter < Blacklight::IndexPresenter
-  def label(field_or_string_or_proc, opts = {})
+  def fields
     original = super
     # To hide label for publication statement
-    @configuration.index_fields['publication_display_ssm'].label = nil
+    configuration.index_fields['publication_display_ssm'].label = nil
 
     original
   end
