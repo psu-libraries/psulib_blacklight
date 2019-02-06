3eb08d66dc2901f2a1809dfdbb1c636d062e3dc8
Charlie Morris <523381+cdmo@users.noreply.github.com>
2019-02-06 15:56:08 -0500

    Make coveralls happy

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -7,14 +7,21 @@ RSpec.describe ApplicationHelper, type: :helper do
     let (:field_data) { ['{"bound_catkey": "355035", "bound_title": '\
         '"The high-caste Hindu woman / With introduction by Rachel L. Bodley", "bound_format": '\
         '"Microfilm, Microfiche, etc.", "bound_callnumber": "AY67.N5W7 1922-24"}'] }
+    let (:just_title_data) { ['{"bound_title": "Bound in: The Tale of The Blah Blah Blah"}'] }
     let (:bound_info_doc) { { value: field_data } }
+    let (:bound_info_lite_doc) { { value: just_title_data } }
 
-    context 'when there is a single value for a link field' do
-      it 'assembles a link' do
+    context 'when there is a full compliment of field data about bound with' do
+      it 'assembles all the info correctly including a link' do
         link_text = '<span>AY67.N5W7 1922-24 (Microfilm, Microfiche, etc.) bound in <a href="/catalog/355035">The high'\
                     '-caste Hindu woman / With introduction by Rachel L. Bodley</a></span>'
         link = bound_info bound_info_doc
         expect(link).to eql link_text
+      end
+    end
+    context 'when there is only a title (subfield a)' do
+      it 'just prints that subfield' do
+        expect(bound_info(bound_info_lite_doc)).to eql '<span>Bound in: The Tale of The Blah Blah Blah</span>'
       end
     end
   end
