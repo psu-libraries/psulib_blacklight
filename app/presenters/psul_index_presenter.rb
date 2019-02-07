0cfd52e76495a3147a20f8d2eac396a03f3e87c2
Banu Hapeloglu Kutlu <bzk60@psu.edu>
2019-02-07 11:00:55 -0500

    #204 publication fields (#221)
    * Index and display publication fields #204
    * To hide the publication statement label in search results, added presenter and override the partial (there might be a better way to solve this, potential candidate for refactor)
    * Reordered the single item labels

---
+++ app/presenters/psul_index_presenter.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class PsulIndexPresenter < Blacklight::IndexPresenter
+  def fields
+    original = super
+    # To hide label for publication statement
+    configuration.index_fields['publication_display_ssm'].label = nil
+
+    original
+  end
+end
---
+++ spec/presenters/psul_index_presenter_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe PsulIndexPresenter do
+  subject(:presenter) { described_class.new(document, request_context, config) }
+
+  let(:document) { SolrDocument.new }
+  let(:request_context) { instance_double(ActionView::Base, document_index_view_type: 'list') }
+  let(:config) { Blacklight::Configuration.new }
+
+  describe '#label' do
+    context 'when a document has a publication statement' do
+      let(:document) do
+        SolrDocument.new(id: 1,
+                         format: 'Book',
+                         publication_display_ssm: 'This is the publication statement of the document')
+      end
+
+      let(:config) do
+        Blacklight::Configuration.new.configure do |config|
+          config.add_index_field 'publication_display_ssm'
+          config.add_index_field 'format'
+        end
+      end
+
+      it 'has no label for publication statement, but still has a label for format' do
+        expect(presenter.fields['publication_display_ssm'].label).to be_nil
+        expect(presenter.fields['format'].label).to eq('Format')
+      end
+    end
+  end
+end
