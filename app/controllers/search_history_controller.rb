d625012256ef5869091579ff45374e32b5829270
Banu Kutlu <bzk60@psu.edu>
2019-03-22 15:55:50 -0400

    WIP: adding blacklight_range_limit gem

--- app/controllers/search_history_controller.rb
+++ app/controllers/search_history_controller.rb
@@ -4,4 +4,6 @@ class SearchHistoryController < ApplicationController
   include Blacklight::SearchHistory
 
   helper BlacklightAdvancedSearch::RenderConstraintsOverride
+  helper BlacklightRangeLimit::ViewHelperOverride
+  helper RangeLimitHelper
 end
