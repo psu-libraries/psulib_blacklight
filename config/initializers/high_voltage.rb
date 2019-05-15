0ffe84f404c6252ff369410d667539e63dd490f8
Charlie Morris <523381+cdmo@users.noreply.github.com>
2019-05-15 11:24:14 -0400

    307 pages (#308)
    * Adds high_voltage gem for static pages
    * Adding first drafts of about and search tips pages.

---
+++ config/initializers/high_voltage.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# config/initializers/high_voltage.rb
+HighVoltage.route_drawer = HighVoltage::RouteDrawers::Root
+
+# Default Layout
+HighVoltage.layout = 'blacklight'
