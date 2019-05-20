85cfc943cc65a7c6f68d50a37a9711c17bf6bb4f
Banu Kutlu <bzk60@psu.edu>
2019-05-20 10:10:17 -0400

    overriding layout_helper for wider sidebar for facets

---
+++ app/helpers/layout_helper.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module LayoutHelper
+  include Blacklight::LayoutHelperBehavior
+    ##
+    # Classes used for sizing the main content of a Blacklight page
+    # @return [String]
+    def main_content_classes
+      'col-md-8'
+    end
+
+    ##
+    # Classes used for sizing the sidebar content of a Blacklight page
+    # @return [String]
+    def sidebar_classes
+      'page-sidebar col-md-4'
+    end
+end
