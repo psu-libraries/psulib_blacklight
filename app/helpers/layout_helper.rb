c7f6d1c24faa26c103e43a8eeb3fe4743dd5b775
Banu Kutlu <bzk60@psu.edu>
2019-05-20 10:11:04 -0400

    :cop:

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -2,17 +2,17 @@
 
 module LayoutHelper
   include Blacklight::LayoutHelperBehavior
-    ##
-    # Classes used for sizing the main content of a Blacklight page
-    # @return [String]
-    def main_content_classes
-      'col-md-8'
-    end
+  ##
+  # Classes used for sizing the main content of a Blacklight page
+  # @return [String]
+  def main_content_classes
+    'col-md-8'
+  end
 
-    ##
-    # Classes used for sizing the sidebar content of a Blacklight page
-    # @return [String]
-    def sidebar_classes
-      'page-sidebar col-md-4'
-    end
+  ##
+  # Classes used for sizing the sidebar content of a Blacklight page
+  # @return [String]
+  def sidebar_classes
+    'page-sidebar col-md-4'
+  end
 end
