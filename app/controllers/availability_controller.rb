c684509fc6fe94892cd88f63b13db5b0cb48eb8f
Banu Kutlu <bzk60@psu.edu>
2019-05-21 16:49:21 -0400

    loading locations json with webpacker

--- app/controllers/availability_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AvailabilityController < ApplicationController
-  def index
-    render json: JSON.parse(File.read('config/locations.json')), layout: false
-  end
-end
--- app/javascript/psulib_blacklight/javascripts/availability.js
+++ app/javascript/psulib_blacklight/javascripts/availability.js
@@ -2,109 +2,94 @@
  * @file
  * Real Time Availability
  */
+import locations from './locations.json';
 
 $(document).on('turbolinks:load', function() {
-    loadAvailability();
+    loadAvailability(locations);
 });
-
-/**
- * Load locations
- *
- * @returns {Promise}
- */
-loadLocations = function () {
-    return new Promise (function(resolve, reject) {
-        $.getJSON('locations', function(data) {
-            resolve(data);
-        });
-    });
-};
 
 /**
  * Load real time holdings and availability info from Sirsi Web Services
  */
-loadAvailability = function () {
+function loadAvailability(locations) {
     // Load Sirsi locations
-    loadLocations().then(function(locations) {
-        all_locations = locations.all_locations;
-        all_libraries = locations.libraries;
-        titleIDs = "";
+    var all_locations = locations.locations;
+    var all_libraries = locations.libraries;
+    var titleIDs = "";
 
-        // Sirsi Web Services Availability url
-        url = 'sirsi url here';
-
-        $('.availability').each(function() {
-            // Get the catkeys
-            catkeys = $(this).attr("data-keys");
-            // Format catkeys to compose titleID params
-            titleIDs += '&titleID=' + catkeys;
-        });
-
-        if (titleIDs.length > 0) {
-            $.get(url + titleIDs, function (xml) {
-                $(xml).find('TitleInfo').each(function () {
-                    holdings = [];
-                    libraries = [];
-                    titleID = $(this).children('titleID').text();
-                    totalCopiesAvailable = parseInt($(this).find("totalCopiesAvailable").text(), 10);
-
-                    $(this).children('CallInfo').each(function () {
-                        libraryID = $(this).children('libraryID').text();
-                        library = (libraryID in all_libraries) ? all_libraries[libraryID] : "";
-                        callNumber = $(this).children('callNumber').text();
-                        numberOfCopies = $(this).children('numberOfCopies').text();
-
-                        // Only for not online items (online items uses 856 urls for display)
-                        if (libraryID.toUpperCase() !== 'ONLINE') {
-                            libAndCount = []
-                            libAndCount['library'] = library
-                            libAndCount['numberOfCopies'] = numberOfCopies
-                            libraries.push(libAndCount);
-
-                            // Holdings
-                            $(this).children("ItemInfo").each(function () {
-                                currentLocationID = $(this).children("currentLocationID").text().toUpperCase();
-                                homeLocationID = $(this).children("homeLocationID").text().toUpperCase();
-                                chargeable = $(this).children("chargeable").text();
-                                status = resolveStatus(chargeable, homeLocationID, currentLocationID);
-
-                                var location = (homeLocationID in all_locations) ? all_locations[homeLocationID] : "";
-                                holdings.push({
-                                    library: library,
-                                    location: location,
-                                    callNumber: callNumber,
-                                    status: status
-                                });
-                            });
-                        }
-                    });
-
-                    $('.availability[data-keys="' + titleID +'"]').each(function () {
-                        availability = $(this);
-                        availabilityButton = availability.find('.availability-button');
-                        availabilityHoldingsPlaceHolder = availability.find('.availability-holdings');
-                        availabilitySnippetPlaceHolder = availability.find('.availability-snippet');
-
-                        // If at least one physical copy, then display availability and holding info
-                        if (Object.keys(holdings).length > 0) {
-                            availabilityButton.removeClass("invisible").addClass("visible");
-                            rawHoldings = groupByLibrary(holdings);
-                            availabilityStructuredData = availabilityDataStructurer(rawHoldings, libraries);
-                            availabilityHoldingsPlaceHolder.html(printAvailabilityData(availabilityStructuredData));
-                            availabilitySnippet(availabilitySnippetPlaceHolder, availabilityStructuredData, totalCopiesAvailable);
-                        } else {
-                            availability.addClass("invisible");
-                        }
-                    });
-
-                });
-            }, "xml");
-        }
+    // Sirsi Web Services Availability url
+    var url = 'sirsi url here';
+    $('.availability').each(function() {
+        // Get the catkeys
+        var catkeys = $(this).attr("data-keys");
+        // Format catkeys to compose titleID params
+        titleIDs += '&titleID=' + catkeys;
     });
-};
+
+    if (titleIDs.length > 0) {
+        $.get(url + titleIDs, function (xml) {
+            $(xml).find('TitleInfo').each(function () {
+                var holdings = [];
+                var libraries = [];
+                var titleID = $(this).children('titleID').text();
+                var totalCopiesAvailable = parseInt($(this).find("totalCopiesAvailable").text(), 10);
+
+                $(this).children('CallInfo').each(function () {
+                    var libraryID = $(this).children('libraryID').text();
+                    var library = (libraryID in all_libraries) ? all_libraries[libraryID] : "";
+                    var callNumber = $(this).children('callNumber').text();
+                    var numberOfCopies = $(this).children('numberOfCopies').text();
+
+                    // Only for not online items (online items uses 856 urls for display)
+                    if (libraryID.toUpperCase() !== 'ONLINE') {
+                        var libAndCount = [];
+                        libAndCount['library'] = library;
+                        libAndCount['numberOfCopies'] = numberOfCopies;
+                        libraries.push(libAndCount);
+
+                        // Holdings
+                        $(this).children("ItemInfo").each(function () {
+                            var currentLocationID = $(this).children("currentLocationID").text().toUpperCase();
+                            var homeLocationID = $(this).children("homeLocationID").text().toUpperCase();
+                            var chargeable = $(this).children("chargeable").text();
+                            var status = resolveStatus(chargeable, homeLocationID, currentLocationID);
+
+                            var location = (homeLocationID in all_locations) ? all_locations[homeLocationID] : "";
+                            holdings.push({
+                                library: library,
+                                location: location,
+                                callNumber: callNumber,
+                                status: status
+                            });
+                        });
+                    }
+                });
+
+                $('.availability[data-keys="' + titleID +'"]').each(function () {
+                    var availability = $(this);
+                    var availabilityButton = availability.find('.availability-button');
+                    var availabilityHoldingsPlaceHolder = availability.find('.availability-holdings');
+                    var availabilitySnippetPlaceHolder = availability.find('.availability-snippet');
+
+                    // If at least one physical copy, then display availability and holding info
+                    if (Object.keys(holdings).length > 0) {
+                        availabilityButton.removeClass("invisible").addClass("visible");
+                        var rawHoldings = groupByLibrary(holdings);
+                        var availabilityStructuredData = availabilityDataStructurer(rawHoldings, libraries);
+                        availabilityHoldingsPlaceHolder.html(printAvailabilityData(availabilityStructuredData));
+                        availabilitySnippet(availabilitySnippetPlaceHolder, availabilityStructuredData, totalCopiesAvailable);
+                    } else {
+                        availability.addClass("invisible");
+                    }
+                });
+
+            });
+        }, "xml");
+    }
+}
 
 function printAvailabilityData(availabilityData) {
-    markupForHoldings = '';
+    var markupForHoldings = '';
     availabilityData.forEach(function(element) {
         markupForHoldings += `
                                 <h4>${element.summary.library} (${element.summary.countAtLibrary} ${element.summary.pluralize})</h4>
@@ -134,9 +119,9 @@ function printAvailabilityData(availabilityData) {
 }
 
 function librariesText(holdingData){
-    libraries = [];
+    var libraries = [];
 
-    for (index in holdingData) {
+    for (var index in holdingData) {
         libraries.push(holdingData[index].summary.library);
     }
 
@@ -144,6 +129,7 @@ function librariesText(holdingData){
 }
 
 function availabilitySnippet(availabilitySnippetPlaceHolder, holdingData, totalCopiesAvailable) {
+    var snippet = "";
     if (totalCopiesAvailable > 0) {
         snippet = holdingData.length > 2 ? 'Multiple Locations' : librariesText(holdingData);
     }
@@ -155,7 +141,10 @@ function availabilitySnippet(availabilitySnippetPlaceHolder, holdingData, totalC
 }
 
 function availabilityDataStructurer(holdingMetadata, availableCountInLibraries) {
-    availabilityStructuredData = [];
+    var availabilityStructuredData = [];
+    var holdingData = [];
+    var pluralize = "";
+
     if (Object.keys(holdingMetadata).length > 0) {
         Object.keys(holdingMetadata).forEach(function (library, index){
             pluralize = (holdingMetadata[library].length > 1) ? 'items' : 'item';
@@ -191,6 +180,7 @@ function groupByLibrary(holdings) {
 }
 
 function resolveStatus(chargeable, homeLocationID, currentLocationID) {
+    var status = "";
     if (chargeable === 'true') {
         status = homeLocationID !== 'ON-ORDER' ? 'Available' : 'Being Acquired by the Library';
     }
