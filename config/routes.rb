b63fc52a2b4f6cad1fce3f7f72ebaf58933ad570
Banu Kutlu <bzk60@psu.edu>
2019-05-23 15:49:25 -0400

    marc view page title change

--- config/routes.rb
+++ config/routes.rb
@@ -38,5 +38,5 @@ Rails.application.routes.draw do
   match '/422' => 'errors#not_found', via: :all
   match '/500' => 'errors#internal_server_error', via: :all
 
-  get 'catalog/:id/librarian_view', to: 'catalog#librarian_view', as: 'marc_view'
+  get 'catalog/:id/marc_view', to: 'catalog#librarian_view', as: 'marc_view'
 end
