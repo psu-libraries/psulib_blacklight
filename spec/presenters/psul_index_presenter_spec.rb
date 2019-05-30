6869976bf1b700b1c4e88df292cd8f40fb844cb7
Banu Kutlu <bzk60@psu.edu>
2019-05-30 10:40:18 -0400

    :cop:

--- spec/presenters/psul_index_presenter_spec.rb
+++ spec/presenters/psul_index_presenter_spec.rb
@@ -12,48 +12,46 @@ RSpec.describe PsulIndexPresenter do
   let(:config) { Blacklight::Configuration.new }
 
   describe '#fields' do
-    context 'on search results view' do
-      let(:document) do
-        SolrDocument.new(id: 1,
-                         author_person_display_ssm: 'Author 1',
-                         author_corp_display_ssm: 'Author 2',
-                         author_meeting_display_ssm: 'Author 3',
-                         format: 'Book',
-                         publication_display_ssm: 'This is the publication statement',
-                         edition_display_ssm: 'This is the edition',
-                         url_fulltext_display_ssm: 'some url')
-      end
+    let(:document) do
+      SolrDocument.new(id: 1,
+                       author_person_display_ssm: 'Author 1',
+                       author_corp_display_ssm: 'Author 2',
+                       author_meeting_display_ssm: 'Author 3',
+                       format: 'Book',
+                       publication_display_ssm: 'This is the publication statement',
+                       edition_display_ssm: 'This is the edition',
+                       url_fulltext_display_ssm: 'some url')
+    end
 
-      let(:config) do
-        Blacklight::Configuration.new.configure do |config|
-          config.add_index_field 'author_person_display_ssm'
-          config.add_index_field 'author_corp_display_ssm'
-          config.add_index_field 'author_meeting_display_ssm'
-          config.add_index_field 'format'
-          config.add_index_field 'publication_display_ssm'
-          config.add_index_field 'edition_display_ssm'
-          config.add_index_field 'url_fulltext_display_ssm'
-        end
+    let(:config) do
+      Blacklight::Configuration.new.configure do |config|
+        config.add_index_field 'author_person_display_ssm'
+        config.add_index_field 'author_corp_display_ssm'
+        config.add_index_field 'author_meeting_display_ssm'
+        config.add_index_field 'format'
+        config.add_index_field 'publication_display_ssm'
+        config.add_index_field 'edition_display_ssm'
+        config.add_index_field 'url_fulltext_display_ssm'
       end
+    end
 
-      it 'has no label for author fields' do
-        expect(presenter.fields['author_person_display_ssm'].label).to be_nil
-        expect(presenter.fields['author_corp_display_ssm'].label).to be_nil
-        expect(presenter.fields['author_meeting_display_ssm'].label).to be_nil
-      end
+    it 'has no label for author fields' do
+      expect(presenter.fields['author_person_display_ssm'].label).to be_nil
+      expect(presenter.fields['author_corp_display_ssm'].label).to be_nil
+      expect(presenter.fields['author_meeting_display_ssm'].label).to be_nil
+    end
 
-      it 'has no label for publication and edition fields' do
-        expect(presenter.fields['publication_display_ssm'].label).to be_nil
-        expect(presenter.fields['edition_display_ssm'].label).to be_nil
-      end
+    it 'has no label for publication and edition fields' do
+      expect(presenter.fields['publication_display_ssm'].label).to be_nil
+      expect(presenter.fields['edition_display_ssm'].label).to be_nil
+    end
 
-      it 'has no label for format' do
-        expect(presenter.fields['format'].label).to be_nil
-      end
+    it 'has no label for format' do
+      expect(presenter.fields['format'].label).to be_nil
+    end
 
-      it 'has a label for URL' do
-        expect(presenter.fields['url_fulltext_display_ssm'].label).to be_not_nil
-      end
+    it 'has a label for URL' do
+      expect(presenter.fields['url_fulltext_display_ssm'].label).to be_not_nil
     end
   end
 end
