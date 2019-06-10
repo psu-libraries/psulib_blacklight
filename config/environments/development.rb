c0b97c2b4fa6bd39609a380df4e93e57fd266114
Banu Kutlu <bzk60@psu.edu>
2019-06-10 10:21:03 -0400

    to fix "ArgumentError - SMTP From address may not be blank: nil:"

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,10 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.action_mailer.default_options = {
+      from: 'noreply@psu.edu'
+  }
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
