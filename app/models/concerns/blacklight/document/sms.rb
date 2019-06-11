01534204f3516b4d8bf5bfb82c5ec569b1f6eef4
Banu Hapeloglu Kutlu <bzk60@psu.edu>
2019-06-11 13:17:16 -0400

    110 email sms fields (#344)
    * adding more fields to email/sms text- #110
    * to fix "ArgumentError - SMTP From address may not be blank: nil:"
    * SMS This to SMS in share dropdown

---
+++ app/models/concerns/blacklight/document/sms.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# This module provides the body of an email export based on the document's semantic values
+module Blacklight::Document::Sms
+  # Return a text string that will be the body of the email
+  def to_sms_text
+    semantics = to_semantic_values
+    body = []
+    ['title', 'latin_title', 'published', 'edition', 'format', 'language'].each do |field|
+      if semantics[field.to_sym].present?
+        value = semantics[field.to_sym]
+        label = "blacklight.sms.text.#{field}"
+        body << I18n.t(label, value: value.join('; '))
+      end
+    end
+    return body.join("\n") unless body.empty?
+  end
+end
--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -2,26 +2,29 @@
 
 class SolrDocument
   include Blacklight::Solr::Document
-  # The following shows how to setup this blacklight document to display marc documents
-  extension_parameters[:marc_source_field] = :marc_display_ss
-  extension_parameters[:marc_format_type] = :marcxml
-  use_extension(Blacklight::Solr::Document::Marc) do |document|
-    document.key?(:marc_display_ss)
-  end
-
-  field_semantics.merge!(
-    title: 'title_display_ssm',
-    language: 'language_facet_ssim',
-    format: 'format'
-  )
 
   # self.unique_key = 'id'
+  field_semantics.merge!(
+    title: 'title_display_ssm',
+    latin_title: 'title_latin_display_ssm',
+    published: 'overall_imprint_display_ssm',
+    edition: 'edition_display_ssm',
+    format: 'format',
+    language: 'language_facet_ssim'
+  )
 
   # Email uses the semantic field mappings below to generate the body of an email.
   SolrDocument.use_extension(Blacklight::Document::Email)
 
   # SMS uses the semantic field mappings below to generate the body of an SMS email.
   SolrDocument.use_extension(Blacklight::Document::Sms)
+
+  # The following shows how to setup this blacklight document to display marc documents
+  extension_parameters[:marc_source_field] = :marc_display_ss
+  extension_parameters[:marc_format_type] = :marcxml
+  use_extension(Blacklight::Solr::Document::Marc) do |document|
+    document.key?(:marc_display_ss)
+  end
 
   # DublinCore uses the semantic field mappings below to assemble an OAI-compliant Dublin Core document
   # Semantic mappings of solr stored fields. Fields may be multi or
