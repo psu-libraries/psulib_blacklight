34dda06a8827ef30bd2561227c9d78aea8311af1
Banu Kutlu <bzk60@psu.edu>
2019-06-12 13:55:01 -0400

    :cop:

--- app/models/record_mailer.rb
+++ app/models/record_mailer.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
+
 # Only works for documents with a #to_marc right now.
 class RecordMailer < ActionMailer::Base
   def email_record(documents, details, url_gen_params)
     title = begin
               documents.first.to_semantic_values[:title].first
-            rescue
+            rescue StandardError
               I18n.t('blacklight.email.text.default_title')
             end
     subject = I18n.t('blacklight.email.text.subject', count: documents.length, title: title)
@@ -13,12 +14,12 @@ class RecordMailer < ActionMailer::Base
     @message        = details[:message]
     @url_gen_params = url_gen_params
 
-    mail(to: details[:to],  subject: subject)
+    mail(to: details[:to], subject: subject)
   end
 
   def sms_record(documents, details, url_gen_params)
     @documents      = documents
     @url_gen_params = url_gen_params
-    mail(to: details[:to], subject: "")
+    mail(to: details[:to], subject: '')
   end
 end
