4ab33a8602095dff905381f53e625776fb84eafa
Charlie Morris <523381+cdmo@users.noreply.github.com>
2019-07-12 10:17:55 -0400

    adding spec for custom 404 routes

---
+++ spec/routing/routing_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Page not found', type: :routing do
+  it 'routes to page not found if path is completely invalid' do
+    expect(get: 'kaljhgl').to route_to controller: 'errors', action: 'not_found', catch_unknown_routes: 'kaljhgl'
+  end
+  it 'routes to page not found if path inside /catalog is not valid' do
+    expect(get: 'catalog/kaljhgl').to route_to controller: 'catalog', action: 'show', id: 'kaljhgl'
+  end
+end
