d3913de3db2707fd2991907080a2065fb5287b5d
Charlie Morris <523381+cdmo@users.noreply.github.com>
2019-08-26 09:37:13 -0400

    Replaces vars with lets

--- app/javascript/psulib_blacklight/javascripts/book_covers.js
+++ app/javascript/psulib_blacklight/javascripts/book_covers.js
@@ -1,10 +1,10 @@
 $(document).on('turbolinks:load', executeBookCovers);
 
 function executeBookCovers() {
-    var isbns_prefixed = [];
+    let isbns_prefixed = [];
 
     $("[data-type='isbn']").each(function(index, thumbnail) {
-        var isbn_values = $(thumbnail).data('isbn');
+        let isbn_values = $(thumbnail).data('isbn');
 
         if (isbn_values) {
             isbns_prefixed.push(isbn_values.map(isbn => "ISBN:" + isbn));
@@ -19,13 +19,13 @@ function executeBookCovers() {
         }
     ).done(
         function(response) {
-            for (var isbn in response) {
+            for (let isbn in response) {
                 response_item = response[isbn];
 
                 if (response_item.hasOwnProperty("thumbnail_url")) {
-                    var type = response_item.bib_key.split(":")[0];
-                    var identifier = response_item.bib_key.split(":")[1];
-                    var thumb_url_zoom_1 = response_item.thumbnail_url.slice(0,-1) + "1"; // instead of zoom of 5
+                    let type = response_item.bib_key.split(":")[0];
+                    let identifier = response_item.bib_key.split(":")[1];
+                    let thumb_url_zoom_1 = response_item.thumbnail_url.slice(0,-1) + "1"; // instead of zoom of 5
                     $('[data-' + type.toLowerCase() + '*="' + identifier + '"]')
                         .replaceWith(`<img class="img-fluid" src="${thumb_url_zoom_1}">`);
                 }
