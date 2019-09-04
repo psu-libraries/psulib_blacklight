4b60b89e8255456d4b5212146914b23aad56f430
Charlie Morris <523381+cdmo@users.noreply.github.com>
2019-09-04 15:57:57 -0400

    :cop:

--- spec/helpers/catalog_helper_spec.rb
+++ spec/helpers/catalog_helper_spec.rb
@@ -89,6 +89,7 @@ RSpec.describe CatalogHelper, type: :helper do
     end
     context 'when a record has bibkeys' do
       let (:document) { { format: ['Book'], isbn_valid_ssm: ['1818181818'] } }
+
       it 'markup sent back containing those bibkeys' do
         thumbnail = render_thumbnail document
         expect(thumbnail).to include 'data-isbn="[&quot;1818181818&quot;]"'
