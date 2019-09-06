cfca2609f6289d4d167b321fc08fb30ca6647bdf
Banu Hapeloglu Kutlu <bzk60@psu.edu>
2019-09-05 22:55:34 -0400

    Update app/controllers/bookmarks_controller.rb
    Co-Authored-By: Charlie Morris <523381+cdmo@users.noreply.github.com>

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,6 +4,6 @@ class BookmarksController < CatalogController
   include Blacklight::Bookmarks
 
   configure_blacklight do |config|
-    _config.index.collection_actions.delete(:sort_widget)
+    config.index.collection_actions.delete(:sort_widget)
   end
 end
