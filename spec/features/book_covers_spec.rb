7866e02ec73a3b47cbcc23cceaf6ead2f11c2922
Charlie Morris <523381+cdmo@users.noreply.github.com>
2019-09-16 11:23:29 -0400

    Replaces example record with one that is actually in the fixtures

--- spec/features/book_covers_spec.rb
+++ spec/features/book_covers_spec.rb
@@ -5,14 +5,14 @@ require 'rails_helper'
 RSpec.feature 'BookCovers', type: :feature do
   describe 'User searches for a book' do
     it 'a book that happens to have a book cover in the Google Books API', js: true do
-      visit '/?utf8=✓&search_field=all_fields&q=The+good+times+AND+0688061702'
+      visit '/?utf8=✓&search_field=all_fields&q=Ethical+and+Social+Issues+in+the+Information+Age+AND+9783319707129'
       expect(page).to have_css('img[src*="https://books.google.com/books/content"]')
     end
   end
 
   describe 'User opens a catalog record page' do
     it 'that happens to have a book cover in the Google Books API', js: true do
-      visit '/catalog/435524'
+      visit '/catalog/22090269'
       expect(page).to have_css('img[src*="https://books.google.com/books/content"]')
     end
   end
