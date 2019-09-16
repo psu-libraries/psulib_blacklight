02e8d3d266890f1ec13e8dcfbce5727f4bf9a7c0
Banu Hapeloglu Kutlu <bzk60@psu.edu>
2019-09-16 16:33:05 -0400

    Update spec/features/book_covers_spec.rb

--- spec/features/book_covers_spec.rb
+++ spec/features/book_covers_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.feature 'BookCovers', type: :feature do
   describe 'User searches for a book' do
-    it 'a book that happens to have a book cover in the Google Books API', js: true do
+    it 'that happens to have a book cover in the Google Books API', js: true do
       visit '/?utf8=✓&search_field=all_fields&q=Ethical+and+Social+Issues+in+the+Information+Age+AND+9783319707129'
       expect(page).to have_css('img[src*="https://books.google.com/books/content"]')
     end
