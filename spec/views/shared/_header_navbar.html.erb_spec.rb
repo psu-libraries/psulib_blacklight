48e71f23a37138559c3f9c567b914c98f5b0678b
Banu Kutlu <bzk60@psu.edu>
2019-10-29 12:58:12 -0400

    adding a spec for hiding search bar from AS

---
+++ spec/views/shared/_header_navbar.html.erb_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'shared/_header_navbar', type: :view do
+  let(:blacklight_config) { Blacklight::Configuration.new }
+
+  before do
+    stub_template 'shared/_user_util_links.html.erb' => 'Some content'
+    stub_template 'catalog/_search_form.html.erb' => 'Other content'
+    assign(:search_bar, Blacklight::SearchBarPresenter.new(controller, blacklight_config))
+  end
+
+  it 'display search bar except on Advanced Search' do
+    render 'shared/header_navbar'
+    expect(rendered).to have_selector '.navbar-search'
+  end
+
+  it 'does not display search bar on Advanced Search' do
+    assign(:search_fields_for_advanced_search, Blacklight::SearchBarPresenter.new(controller, blacklight_config))
+    render 'shared/header_navbar'
+    expect(rendered).not_to have_selector '.navbar-search'
+  end
+end
