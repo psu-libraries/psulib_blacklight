22fb2af32284374c807d26319e97dbd4a1799c0a
Charlie Morris <523381+cdmo@users.noreply.github.com>
2020-03-20 17:26:36 -0400

    :cop:

--- spec/requests/alerts_spec.rb
+++ spec/requests/alerts_spec.rb
@@ -4,15 +4,22 @@ require 'rails_helper'
 
 RSpec.describe 'Errors', type: :request do
   describe 'alert message' do
-    it 'flashes the alert message when present in blackcat_messages.yml' do
-      file = Tempfile.new('blackcat_messages.yml')
-      file.write("alert: stubbed alert")
-      file.open
-      allow(YAML).to receive(:load_file).and_call_original
-      allow(YAML).to receive(:load_file).with(Rails.root.join('config', 'blackcat_messages.yml')).and_return(file.read)
+    let(:file) { Tempfile.new('blackcat_messages.yml') }
 
-      get root_path
-      expect(flash[:error]).to match('stubbed alert')
+    context 'when there is a value present for the "alert" key' do
+      before do
+        file.write('alert: stubbed alert')
+        file.open
+        allow(YAML).to receive(:load_file).and_call_original
+        allow(YAML).to receive(:load_file)
+          .with(Rails.root.join('config', 'blackcat_messages.yml'))
+          .and_return(file.read)
+      end
+
+      it 'flashes the alert message when present in blackcat_messages.yml' do
+        get root_path
+        expect(flash[:error]).to match('stubbed alert')
+      end
     end
   end
 end
