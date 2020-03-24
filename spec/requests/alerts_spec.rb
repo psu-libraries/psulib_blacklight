3af8d01d03bedef712cebf2471734b26f52067ad
Charlie Morris <523381+cdmo@users.noreply.github.com>
2020-03-24 16:30:11 -0400

    Attempt again, avoiding before block, but I don't think this will work

--- spec/requests/alerts_spec.rb
+++ spec/requests/alerts_spec.rb
@@ -5,14 +5,11 @@ require 'rails_helper'
 RSpec.describe 'Blackcat Messages', type: :request do
   describe 'alert message' do
     context 'when there is a value present for the "alert" key' do
-      before do
+      it 'flashes the alert message when present in blackcat_messages.yml' do
         allow(YAML).to receive(:load_file).and_call_original
         allow(YAML).to receive(:load_file)
           .with(Rails.root.join('config', 'blackcat_messages.yml'))
           .and_return(YAML.safe_load('alert: stubbed alert'))
-      end
-
-      it 'flashes the alert message when present in blackcat_messages.yml' do
         get root_path
         expect(flash[:error]).to match('stubbed alert')
       end
