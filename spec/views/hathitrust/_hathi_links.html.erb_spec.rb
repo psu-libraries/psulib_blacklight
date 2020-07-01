d3c31ae66f2b9e4bf277b785f654adb9ad40fb65
Banu Kutlu <bzk60@psu.edu>
2020-07-01 16:06:40 -0400

    added a view spec, added hathi partial to show fields

---
+++ spec/views/hathitrust/_hathi_links.html.erb_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'hathitrust/hathi_links', type: :view do
+  let(:hathi_fields) {
+    {
+      text: I18n.t('blackcat.hathitrust.public_domain_text'),
+      url: 'https://catalog.hathitrust.org/Record/12345',
+      additional_text: I18n.t('blackcat.hathitrust.etas_additional_text')
+    }
+  }
+
+  it 'renders a hathi logo linked to hathi record with correct text' do
+    render 'hathitrust/hathi_links', hathi_links: hathi_fields
+
+    expect(rendered).to have_link(href: 'https://catalog.hathitrust.org/Record/12345')
+      .and have_css("img[src*='HathiTrust_logo']")
+      .and include(I18n.t('blackcat.hathitrust.public_domain_text'))
+      .and include I18n.t('blackcat.hathitrust.etas_additional_text')
+  end
+end
