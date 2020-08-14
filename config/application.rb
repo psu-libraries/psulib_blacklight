671fbb74d38f126ed7275b5a5fa2130aa98c2427
Charlie Morris <523381+cdmo@users.noreply.github.com>
2020-08-14 11:45:24 -0400

    Rails 6 upgrade (#577)
    * Most changes from Rails upgrade command at https://edgeguides.rubyonrails.org/upgrading_ruby_on_rails.html#upgrading-from-rails-5-2-to-rails-6-0
    * Sets autoload to class and updates rspec-rails (older version had a bug with Rails 6)
    * Updates blacklight-marc gem

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,10 @@ Bundler.require(*Rails.groups)
 module Psulcat
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults '6.0'
+
+    # Should be zeitwerk, but we need to upgrade to Blacklight > 7.9 first. Once we have that we can delete this line.
+    config.autoloader = :classic
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
