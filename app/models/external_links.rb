9be2b64388c744b55b0e3d4b15c6d48458f65f18
Banu Kutlu <bzk60@psu.edu>
2021-05-10 22:38:24 -0400

    refactoring

--- app/models/external_links.rb
+++ app/models/external_links.rb
@@ -4,21 +4,13 @@ module ExternalLinks
   def psu_digital_collections_links
     return if full_links.blank?
 
-    parsed_links.select { |link| digital_collections_link? link }
-      .each do |link|
-      link['prefix'] = link_prefix link
-      link['notes'] = link_notes link
-    end
+    all_links.select { |link| digital_collections_link? link }
   end
 
   def access_online_links
     return if full_links.blank?
 
-    parsed_links.reject { |link| digital_collections_link? link }
-      .each do |link|
-        link['prefix'] = link_prefix link
-        link['notes'] = link_notes link
-      end
+    all_links.reject { |link| digital_collections_link? link }
   end
 
   private
@@ -29,6 +21,13 @@ module ExternalLinks
 
     def parsed_links
       full_links.map { |item| JSON.parse item }
+    end
+
+    def all_links
+      parsed_links.each do |link|
+        link['prefix'] = link_prefix link
+        link['notes'] = link_notes link
+      end
     end
 
     def digital_collections_link?(link)
