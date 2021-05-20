769a356858d78f0dc25f40b3c23757e66c6e9813
Banu Kutlu <bzk60@psu.edu>
2021-05-20 09:25:05 -0400

    need to explicitly define facet queries  (#746)
    * need to explicitly define facet queries, see here https://github.com/projectblacklight/blacklight/issues/2401

---
+++ spec/features/facet_queries_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature 'Facet Queries', type: :feature do
+  it 'All Authors Facet' do
+    visit '/?f%5Ball_authors_facet%5D%5B%5D=Chanukoff%2C+L.%2C+1892-1958'
+    expect(page).to have_selector '.filter-name',
+                                  exact_text: 'All Authors Facet'
+    expect(page).to have_selector '.filter-value',
+                                  exact_text: 'Chanukoff, L., 1892-1958'
+    expect(page).to have_selector 'article[data-document-id="3750004"]'
+  end
+
+  it 'Subject Facet' do
+    visit '/?f[subject_facet][]=Islamic+decorative+arts%E2%80%94Turkey'
+    expect(page).to have_selector '.filter-name',
+                                  exact_text: 'Subject Facet'
+    expect(page).to have_selector '.filter-value',
+                                  exact_text: 'Islamic decorative arts—Turkey'
+    expect(page).to have_selector 'article[data-document-id="21322677"]'
+  end
+end
