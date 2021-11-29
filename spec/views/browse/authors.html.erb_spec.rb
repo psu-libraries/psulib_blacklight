605b76a94c09355408dfe3aaeec4a59bb4ea7efd
Nick Tiberi <nicktiberi@gmail.com>
2021-11-29 12:07:39 -0500

    More specific error text for no results on author/subject browse (#916)
    If a user browses by author or subject and gets no results, we now
    display some helpful hints that can help them refine their search.

--- spec/views/browse/authors.html.erb_spec.rb
+++ spec/views/browse/authors.html.erb_spec.rb
@@ -11,6 +11,8 @@ RSpec.describe 'browse/authors', type: :view do
 
   it 'renders an error message when there are no items to show' do
     expect(rendered).not_to have_selector 'table'
-    expect(rendered).to have_selector '.alert-warning h2', text: 'No records found.'
+    expect(rendered).to have_selector '.alert-warning h4',
+                                      text: 'No authors found. Try the following tips to revise your search:'
+    expect(rendered).to have_selector 'ol'
   end
 end
