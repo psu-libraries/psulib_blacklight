b24c02b12080fe8efad9f4c7bad95abebd13af64
Alex Kiessling <ajkiessl@gmail.com>
2022-04-05 13:03:22 -0400

    Reverted some things.  We don't want to completely remove all the etas stuff.  Just alter logic to give google link if etas seting is turned off and the ht access is deny.

--- app/models/hathi_links.rb
+++ app/models/hathi_links.rb
@@ -2,12 +2,14 @@
 
 module HathiLinks
   def hathi_links
-    return nil if oclc_number.blank?
+    return nil if ht_access.blank?
 
     {
       text: ht_link_text,
       url: ht_url,
-      hathitrust: hathitrust?
+      additional_text: ht_additional_text,
+      etas_item: etas_item?,
+      open_ht_access: open_ht_access?
     }
   end
 
@@ -18,22 +20,31 @@ module HathiLinks
     end
 
     def ht_url
-      return "https://catalog.hathitrust.org/api/volumes/oclc/#{oclc_number}.html" if hathitrust?
+      ocn = @_source['oclc_number_ssim'].first
+      suffix = etas_item? ? I18n.t('blackcat.hathitrust.url_append') : ''
+      return "https://catalog.hathitrust.org/api/volumes/oclc/#{ocn}.html#{suffix}" if open_ht_access? || etas_item?
 
-      "https://google.com/books?vid=OCLC#{oclc_number}"
+      "https://google.com/books?vid=OCLC#{ocn}"
     end
 
     def ht_link_text
-      return I18n.t('blackcat.hathitrust.public_domain_text') if hathitrust?
+      return I18n.t('blackcat.hathitrust.public_domain_text') if open_ht_access?
+      return I18n.t('blackcat.hathitrust.restricted_access_text') unless etas_item?
 
-      I18n.t('blackcat.hathitrust.alt_hathi_text')
+      I18n.t('blackcat.hathitrust.etas_text')
     end
 
-    def hathitrust?
-      ht_access == 'allow'
+    def ht_additional_text
+      return nil unless etas_item?
+
+      I18n.t('blackcat.hathitrust.etas_additional_text')
     end
 
-    def oclc_number
-      @_source['oclc_number_ssim']&.first
+    def etas_item?
+      ht_access == 'deny' && Settings&.hathi_etas
+    end
+
+    def open_ht_access?
+      @_source['ht_access_ss'] == 'allow'
     end
 end
--- spec/components/external_links/hathi_links_component.html.erb_spec.rb
+++ spec/components/external_links/hathi_links_component.html.erb_spec.rb
@@ -7,12 +7,14 @@ RSpec.describe ExternalLinks::HathiLinksComponent, type: :component do
     render_inline(described_class.new(links: hathi_links))
   end
 
-  context 'when hathi_links #hathitrust is true' do
+  context 'when hathi_links is open_ht_access but not an etas_item' do
     let(:hathi_links) {
       {
         text: I18n.t('blackcat.hathitrust.public_domain_text'),
         url: 'https://catalog.hathitrust.org/Record/12345',
-        hathitrust: true
+        additional_text: nil,
+        etas_item: false,
+        open_ht_access: true
       }
     }
 
@@ -20,22 +22,53 @@ RSpec.describe ExternalLinks::HathiLinksComponent, type: :component do
       expect(rendered).to have_link(href: 'https://catalog.hathitrust.org/Record/12345')
         .and have_css("img[src*='HathiTrust_logo']")
         .and have_text(I18n.t('blackcat.hathitrust.public_domain_text'))
+      expect(rendered).not_to have_content(I18n.t('blackcat.hathitrust.etas_additional_text'))
     end
   end
 
-  context 'when hathi_links #hathitrust is false' do
+  context 'when hathi_links is open_ht_access and an etas_item' do
     let(:hathi_links) {
       {
-        text: I18n.t('blackcat.hathitrust.alt_hathi_text'),
+        text: I18n.t('blackcat.hathitrust.etas_text'),
+        url: 'https://catalog.hathitrust.org/Record/12345',
+        additional_text: I18n.t('blackcat.hathitrust.etas_additional_text'),
+        etas_item: true,
+        open_ht_access: true
+      }
+    }
+
+    it 'renders a hathi logo linked to hathi record with correct text' do
+      expect(rendered).to have_link(href: 'https://catalog.hathitrust.org/Record/12345')
+        .and have_css("img[src*='HathiTrust_logo']")
+        .and have_text(I18n.t('blackcat.hathitrust.etas_text'))
+      expect(rendered).to have_content(I18n.t('blackcat.hathitrust.etas_additional_text'))
+      expect(rendered).to have_content('Full Text available online')
+    end
+  end
+
+  context 'when hathi_links is neither open_ht_access nor an etas_item' do
+    let(:hathi_links) {
+      {
+        text: I18n.t('blackcat.hathitrust.restricted_access_text'),
         url: 'https://google.com/books?vid=OCLC12345',
-        hathitrust: false
+        additional_text: nil,
+        etas_item: false,
+        open_ht_access: false
       }
     }
 
     it 'renders a google books logo linked to google books record with correct text' do
       expect(rendered).to have_link(href: 'https://google.com/books?vid=OCLC12345')
       expect(rendered).to have_css("img[src*='google_books_logo']")
-      expect(rendered).to have_text(I18n.t('blackcat.hathitrust.alt_hathi_text'))
+      expect(rendered).to have_text(I18n.t('blackcat.hathitrust.restricted_access_text'))
+    end
+  end
+
+  context 'when hathi_links is nil' do
+    let(:hathi_links) { nil }
+
+    it 'renders nothing' do
+      expect(rendered.to_s).to eq ""
     end
   end
 end
