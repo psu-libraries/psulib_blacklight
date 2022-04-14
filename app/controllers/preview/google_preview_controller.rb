a269d9a653470e1dab3c914764773d59bed69289
Alex Kiessling <ajkiessl@gmail.com>
2022-04-14 13:19:37 -0400

    Proxy endpoint for sending google preview request.

---
+++ app/controllers/preview/google_preview_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'net/http'
+
+module Preview
+  class GooglePreviewController < ActionController::API
+    def data
+      render json: json_response
+    end
+
+    private
+
+      def json_response
+        JSON.parse(send_request.gsub('var _GBSBookInfo = ', '')[0..-2])
+      end
+
+      def send_request
+        Net::HTTP.get(uri)
+      end
+
+      def uri
+        URI("https://books.google.com/books?jscmd=viewapi&bibkeys=#{search_item}")
+      end
+
+      def search_item
+        params["search_item"]
+      end
+  end
+end
--- app/javascript/psulib_blacklight/javascripts/google_preview.js
+++ app/javascript/psulib_blacklight/javascripts/google_preview.js
@@ -1,28 +1,26 @@
 $(document).ready(() => {
   const element = document.getElementById('google-preview');
-
-  // Function to process GBS info and update the dom.
-  window.ProcessGBSBookInfo = function (booksInfo) {
-    for (const key in booksInfo) {
-      const bookInfo = booksInfo[key];
-      if (bookInfo) {
-        element.href = bookInfo.preview_url;
-        if (bookInfo.preview === 'full') {
-          element.innerHTML += 'Full View in Google Books';
-          element.style.display = '';
-        } else if (bookInfo.preview === 'partial') {
-          element.innerHTML += 'Preview in Google Books';
-          element.style.display = '';
-        }
-      }
-    }
-  };
+  const googlePreviewUrl = '/preview/google-preview-data';
+  const googlePreviewUrlQuery = googlePreviewUrl + '/?' + 'search_item=' + element.attributes.data.value.toString();
 
   if (element) {
-    const searchItem = element.attributes.data.value;
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = `//books.google.com/books?jscmd=viewapi&bibkeys=${searchItem}&callback=ProcessGBSBookInfo`;
-    document.getElementsByTagName('head')[0].appendChild(script);
+    $.ajax({
+      url: googlePreviewUrlQuery,
+      success: function (response) {
+        for (const key in response) {
+          const bookInfo = response[key];
+          if (bookInfo) {
+            element.firstElementChild.href = bookInfo.preview_url;
+            if (bookInfo.preview === 'full') {
+              element.firstElementChild.innerHTML += 'Full View in Google Books';
+              element.style.display = '';
+            } else if (bookInfo.preview === 'partial') {
+              element.firstElementChild.innerHTML += 'Preview in Google Books';
+              element.style.display = '';
+            }
+          }
+        }
+      }
   }
+)}
 });
