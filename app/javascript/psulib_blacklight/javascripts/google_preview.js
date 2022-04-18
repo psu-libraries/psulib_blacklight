29038d89a48a80a0c6eb95b6c9ad291fa0de46a9
Alex Kiessling <ajkiessl@gmail.com>
2022-04-18 12:44:31 -0400

    Changed data attr to be more descriptive on google preview div.  Grab google preview link using search item from google response.  Logic to handle this if no serarch item match.  Test for this.

--- app/javascript/psulib_blacklight/javascripts/google_preview.js
+++ app/javascript/psulib_blacklight/javascripts/google_preview.js
@@ -1,27 +1,35 @@
 $(document).ready(() => {
   const element = document.getElementById('google-preview');
   const googlePreviewUrl = '/preview/google-preview-data';
-  const googlePreviewUrlQuery = `${googlePreviewUrl}/?search_item=${element.attributes.data.value.toString()}`;
+  const searchItem = element.dataset.searchItem;
+  const googlePreviewUrlQuery = `${googlePreviewUrl}/?search_item=${searchItem}`;
 
   if (element) {
     $.ajax({
       url: googlePreviewUrlQuery,
       success(response) {
-        for (const key in response) {
-          const bookInfo = response[key];
-          if (bookInfo) {
-            element.firstElementChild.href = bookInfo.preview_url;
-            if (bookInfo.preview === 'full') {
-              element.firstElementChild.innerHTML +=
-                'Full View in Google Books';
-              element.style.display = '';
-            } else if (bookInfo.preview === 'partial') {
-              element.firstElementChild.innerHTML += 'Preview in Google Books';
-              element.style.display = '';
-            }
+        const bookInfo = response[searchItem];
+        if (bookInfo) {
+          setPreviewButton(bookInfo)
+        } else {
+          for (const key in response) {
+            const bookInfoIter = response[key];
+            setPreviewButton(bookInfoIter)
           }
         }
       },
     });
+  }
+
+  let setPreviewButton = function(bookInfo) {
+    element.firstElementChild.href = bookInfo.preview_url;
+    if (bookInfo.preview === 'full') {
+      element.firstElementChild.innerHTML +=
+          'Full View in Google Books';
+      element.style.display = '';
+    } else if (bookInfo.preview === 'partial') {
+      element.firstElementChild.innerHTML += 'Preview in Google Books';
+      element.style.display = '';
+    }
   }
 });
--- spec/features/google_preview_link_spec.rb
+++ spec/features/google_preview_link_spec.rb
@@ -12,7 +12,7 @@ RSpec.feature 'Google Preview Link', :vcr, type: :feature, js: true do
     it 'does not display preview link' do
       visit 'catalog/3500414'
       expect(page).to have_css("img[src*='gbs_preview_button1']", visible: :hidden)
-        .and have_css("div[data='LCCN:12345']", visible: :hidden)
+        .and have_css("div[data-search-item='LCCN:12345']", visible: :hidden)
     end
   end
 
@@ -42,7 +42,18 @@ RSpec.feature 'Google Preview Link', :vcr, type: :feature, js: true do
     it 'does not display preview link' do
       visit 'catalog/3500414'
       expect(page).to have_css("img[src*='gbs_preview_button1']", visible: :hidden)
-        .and have_css("div[data='LCCN:12345']", visible: :hidden)
+        .and have_css("div[data-search-item='LCCN:12345']", visible: :hidden)
+    end
+  end
+
+  context "when returned result contains a link to a book in Google Books but the search item doesn't match" do
+    it 'displays preview link for the returned result' do
+      visit 'catalog/3500414'
+      expect(page).to have_css("img[src*='gbs_preview_button1']", visible: :visible)
+        .and have_css("div[id='google-preview']", visible: :visible)
+        .and have_css("a[href*='https://books.google.com/books?id=iDuSDwAAQBAJ']",
+                      visible: :visible)
+        .and have_content('Preview in Google Books')
     end
   end
 end
