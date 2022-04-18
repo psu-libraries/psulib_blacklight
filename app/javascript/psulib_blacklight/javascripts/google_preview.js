93144de7d6acd167f229af98e2b2172bbe42166f
Alex Kiessling <ajkiessl@gmail.com>
2022-04-18 13:01:11 -0400

    Linting

--- app/javascript/psulib_blacklight/javascripts/google_preview.js
+++ app/javascript/psulib_blacklight/javascripts/google_preview.js
@@ -1,7 +1,10 @@
 $(document).ready(() => {
   const element = document.getElementById('google-preview');
-  const googlePreviewUrl = '/preview/google-preview-data';
-  const searchItem = element.dataset.searchItem;
+  const urlComponents = {
+    googlePreviewUrl: '/preview/google-preview-data',
+    searchItem: element.dataset.searchItem,
+  };
+  const { googlePreviewUrl, searchItem } = urlComponents;
   const googlePreviewUrlQuery = `${googlePreviewUrl}/?search_item=${searchItem}`;
 
   if (element) {
@@ -10,26 +13,25 @@ $(document).ready(() => {
       success(response) {
         const bookInfo = response[searchItem];
         if (bookInfo) {
-          setPreviewButton(bookInfo)
+          setPreviewButton(bookInfo);
         } else {
           for (const key in response) {
             const bookInfoIter = response[key];
-            setPreviewButton(bookInfoIter)
+            setPreviewButton(bookInfoIter);
           }
         }
       },
     });
   }
 
-  let setPreviewButton = function(bookInfo) {
+  let setPreviewButton = function (bookInfo) {
     element.firstElementChild.href = bookInfo.preview_url;
     if (bookInfo.preview === 'full') {
-      element.firstElementChild.innerHTML +=
-          'Full View in Google Books';
+      element.firstElementChild.innerHTML += 'Full View in Google Books';
       element.style.display = '';
     } else if (bookInfo.preview === 'partial') {
       element.firstElementChild.innerHTML += 'Preview in Google Books';
       element.style.display = '';
     }
-  }
+  };
 });
