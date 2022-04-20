59f44bbc27fefae2486bf69f674456c66b404f72
Alex Kiessling <ajkiessl@gmail.com>
2022-04-20 09:49:44 -0400

    Using .nil? instead of .blank? since the access_facet will only return an array or nil

--- app/components/external_links/google_preview_link_component.rb
+++ app/components/external_links/google_preview_link_component.rb
@@ -34,7 +34,7 @@ module ExternalLinks
 
       def free_to_read?
         access_facet = document[:document]['access_facet']
-        return false if access_facet.blank?
+        return false if access_facet.nil?
 
         access_facet.include?('Free to Read')
       end
