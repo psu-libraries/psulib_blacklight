48767cae8d259115ffed532a27149fcd84f35c3a
Smulligan <smm5878@psu.edu>
2022-10-27 13:05:57 -0400

    started framework for advanced search feature test

---
+++ spec/features/advanced_search_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Advanced Search', type: :feature do
+  describe 'User uses advanced search', js: true do
+    #before do
+        #direct to advance search
+    #end 
+
+    describe 'single term searches', js: true do 
+      #before do
+        #initiate actual search
+      #end 
+
+      context 'when searching by series title' do
+        #before do
+            #search in field 'series' for term
+        #end 
+
+        it 'results include expected CAT keys' do
+          #expect statements for 22861023 and 22861022
+          true
+        end
+      end 
+
+    end     
+  end
+end
