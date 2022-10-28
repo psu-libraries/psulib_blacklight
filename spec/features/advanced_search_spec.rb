74e77b0c3dfc778e13ebdba1cf2322f310292cd0
Smulligan <smm5878@psu.edu>
2022-10-28 19:15:43 -0400

    filled in most specs for advanced search, completed framework for basic search

--- spec/features/advanced_search_spec.rb
+++ spec/features/advanced_search_spec.rb
@@ -5,98 +5,127 @@ require 'rails_helper'
 RSpec.describe 'Advanced Search', type: :feature do
   describe 'User uses advanced search', js: true do
     before do
-        #direct to advance search
+      visit '/advanced'
     end 
 
     describe 'single term searches', js: true do 
-      before do
-        #initiate actual search
-      end 
 
       context 'when searching by series title' do
         before do
-            #search in field 'series' for term: 'Black studies & critical thinking'
+          fill_in 'series', with: 'Yale studies in political science'
+          click_button 'advanced-search-submit'
         end 
 
         it 'results include expected CAT keys' do
-          #expect statements for 22861023 and 22861022
+          expect(page).to have_selector 'article[data-document-id="1373276"]'
         end
       end 
 
       context 'when searching by series keyword' do
         before do
-            #search in field 'series' for term: critical thinking
+          fill_in 'series', with: 'political science'
+          click_button 'advanced-search-submit'
         end 
 
         it 'results include expected CAT keys' do
-          #expect statements for 22861023 and 22861022
+          expect(page).to have_selector 'article[data-document-id="1373276"]'
         end
       end 
 
       context 'when searching by isbn' do
         before do
-            #search in field 'identifiers' for term: 9780820330365 
+            fill_in 'identifiers', with: '9780442290917'
+            click_button 'advanced-search-submit'
         end 
 
         it 'results include expected CAT keys' do
-          #expect statements for 4436334
+          expect(page).to have_selector 'article[data-document-id="1069788"]'
         end
       end 
 
       context 'when searching by issn' do
         before do
-            #search in field 'identifiers' for term 0003-4843 or 00034843
+          fill_in 'identifiers', with: '10756787'
+          click_button 'advanced-search-submit'
         end 
 
         it 'results include expected CAT keys' do
-          #expect statements for 717714
+            expect(page).to have_selector 'article[data-document-id="1443986"]'
+        end
+      end 
+
+      context 'when searching by LCCN' do
+        before do
+          fill_in 'identifiers', with: '2001270122'
+          click_button 'advanced-search-submit'
+        end 
+
+        it 'results include expected CAT keys' do
+            expect(page).to have_selector 'article[data-document-id="2250425"]'
         end
       end 
 
       context 'when searching by publisher' do
         before do
-            #search in field 'publisher' for term 'Peter Lang'
+          fill_in 'publisher', with: 'Norton'
+          click_button 'advanced-search-submit'
         end 
 
         it 'results include expected CAT keys' do
-          #expect statements for 4660904, 1742958, 23794738 (may be pretty far back in search results)
+          expect(page).to have_selector 'article[data-document-id="2069311"]'
         end
       end 
-
+=begin
       context 'when searching by a single publication date' do
         before do
-            #search in field 'range[pub_date_itsi][begin]=' for term '2022'
+            #field 'range[pub_date_itsi][begin]=', with: '2017'
+            click_button 'advanced-search-submit'
         end 
 
         it 'results include expected CAT keys' do
-          #expect statements for 38337883
+          expect(page).to have_selector 'article[data-document-id="31805602"]'
         end
       end 
 
       context 'when searching by publication date range' do
         before do
-            #search in field 'range%5Bpub_date_itsi%5D%5Bbegin%5D=2022&range%5Bpub_date_itsi%5D%5Bend%5D=2023
+            #field 'range%5Bpub_date_itsi%5D%5Bbegin%5D=2017&range%5Bpub_date_itsi%5D%5Bend%5D=2018'
         end 
 
         it 'results include expected CAT keys' do
-          #expect statements for 38337883
+          expect(page).to have_selector 'article[data-document-id="31805602"]'
+          expect(page).to have_selector 'article[data-document-id="31805615"]'
         end
       end 
-
+=end
     end 
     
     describe 'combination searches', js: true do
-      before do
-        #conditions for combination search
-      end  
         
-      context 'when searching by publication date range' do
+      context 'when searching for a specific book about quilts in Cumberland County' do
         before do
-           #search in field 'range%5Bpub_date_itsi%5D%5Bbegin%5D=2022&range%5Bpub_date_itsi%5D%5Bend%5D=2023
-        end 
+          fill_in 'title', with: 'quilts cumberland'
+          fill_in 'author', with: 'quilters'
+          #facets go here
+          click_button 'advanced-search-submit'
+        end  
     
         it 'results include expected CAT keys' do
-          #expect statements for 38337883
+          expect(page).to have_selector 'article[data-document-id="18648055"]'
+        end
+      end 
+
+      context 'when searching for a specific book on geological perspectives of climate change' do
+        before do
+          fill_in 'subject', with: 'Climatology'
+          fill_in 'series', with: 'AAPG studies in geology'
+          fill_in 'publisher', with: 'Petroleum Geologists'
+          #facets go here
+          click_button 'advanced-search-submit'
+        end  
+    
+        it 'results include expected CAT keys' do
+          expect(page).to have_selector 'article[data-document-id="2250425"]'
         end
       end 
     end
---
+++ spec/features/basic_search_spec.rb
@@ -0,0 +1,188 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Basic Search', type: :feature do
+  describe 'User uses basic search', js: true do
+
+    describe 'searching by title', js: true do
+      before do
+        visit '/?search_field=title&q='
+      end
+
+      context 'when searching by title' do
+        before do
+          # switch TITLE with search term
+          fill_in 'q', with: 'TITLE'
+          click_button 'search'
+        end 
+      
+        it 'results include expected CAT keys' do
+          # switch CATKEY with number
+          expect(page).to have_selector 'article[data-document-id="CATKEY"]'
+          expect(page).to have_selector 'article[data-document-id="CATKEY"]'
+        end
+      end
+
+      context 'when searching by title series without quotes' do
+        before do
+          # switch TITLE with search term
+          fill_in 'q', with: 'TITLE'
+          click_button 'search'
+        end 
+        
+        it 'results include expected CAT keys' do
+          # switch CATKEY with number
+          expect(page).to have_selector 'article[data-document-id="CATKEY"]'
+          expect(page).to have_selector 'article[data-document-id="CATKEY"]'
+        end
+      end
+
+      context 'when searching by alternate title' do
+        before do
+          # switch TITLE with search term
+          fill_in 'q', with: 'TITLE'
+          click_button 'search'
+        end 
+        
+        it 'results include expected CAT keys' do
+          # switch CATKEY with number
+          expect(page).to have_selector 'article[data-document-id="CATKEY"]'
+        end
+      end
+
+      context 'when searching by former title' do
+        before do
+          # switch TITLE with search term
+          fill_in 'q', with: 'TITLE'
+          click_button 'search'
+        end 
+        
+        it 'results include expected CAT keys' do
+          # switch CATKEY with number
+          expect(page).to have_selector 'article[data-document-id="CATKEY"]'
+        end
+      end
+
+      context 'when searching by related title' do
+        before do
+          # switch TITLE with search term
+          fill_in 'q', with: 'TITLE'
+          click_button 'search'
+        end 
+        
+        it 'results include expected CAT keys' do
+          # switch CATKEY with number
+          expect(page).to have_selector 'article[data-document-id="CATKEY"]'
+        end
+      end
+    end
+
+    describe 'searching by author', js: true do
+      before do
+        visit '/?search_field=author&q='
+      end
+
+      context 'when searching by person author (natural order)' do
+        before do
+          # switch AUTHOR with search term
+          fill_in 'q', with: 'AUTHOR'
+          click_button 'search'
+        end 
+        
+        it 'results include expected CAT keys' do
+          # switch CATKEY with number
+          expect(page).to have_selector 'article[data-document-id="CATKEY"]'
+          expect(page).to have_selector 'article[data-document-id="CATKEY"]'
+          expect(page).to have_selector 'article[data-document-id="CATKEY"]'
+        end
+      end
+
+      context 'when searching by person author (inverted order)' do
+        before do
+          # switch AUTHOR with search term
+          fill_in 'q', with: 'AUTHOR'
+          click_button 'search'
+        end 
+        
+        it 'results include expected CAT keys' do
+          # switch CATKEY with number
+          expect(page).to have_selector 'article[data-document-id="CATKEY"]'
+          expect(page).to have_selector 'article[data-document-id="CATKEY"]'
+          expect(page).to have_selector 'article[data-document-id="CATKEY"]'
+        end
+      end
+
+      context 'when searching by corporate author' do
+        before do
+          # switch AUTHOR with search term
+          fill_in 'q', with: 'AUTHOR'
+          click_button 'search'
+        end 
+        
+        it 'results include expected CAT keys' do
+          # switch CATKEY with number
+          expect(page).to have_selector 'article[data-document-id="CATKEY"]'
+        end
+      end
+
+      context 'when searching by conference author' do
+        before do
+          # switch AUTHOR with search term
+          fill_in 'q', with: 'AUTHOR'
+          click_button 'search'
+        end 
+        
+        it 'results include expected CAT keys' do
+          # switch CATKEY with number
+          expect(page).to have_selector 'article[data-document-id="CATKEY"]'
+        end
+      end
+
+    end
+    
+    describe 'searching by subject', js: true do
+      before do
+        visit '/?search_field=subject&q='
+      end
+
+      context 'when using a basic search' do
+        before do
+          fill_in 'q', with: 'geology'
+          click_button 'search'
+        end 
+  
+        it 'results include expected CAT keys' do
+          expect(page).to have_selector 'article[data-document-id="2680348"]'
+          expect(page).to have_selector 'article[data-document-id="2724728"]'
+        end
+      end
+
+      context 'when using a fuller search without quotes' do
+        before do
+          fill_in 'q', with: 'African American women'
+          click_button 'search'
+        end 
+  
+        it 'results include expected CAT keys' do
+          expect(page).to have_selector 'article[data-document-id="24053587"]'
+          expect(page).to have_selector 'article[data-document-id="22080733"]'
+        end
+      end
+
+      context 'when using a fuller search without quotes' do
+        before do
+          fill_in 'q', with: '"African American women"'
+          click_button 'search'
+        end 
+  
+        it 'results include expected CAT keys' do
+          expect(page).to have_selector 'article[data-document-id="24053587"]'
+          expect(page).not_to have_selector 'article[data-document-id="22080733"]'
+        end
+      end
+
+
+    end
+  end
+end
