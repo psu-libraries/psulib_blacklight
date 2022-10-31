a7d54b2e35d27ab393128f5ef214566ca39eea23
whereismyjetpack <djb44@psu.edu>
2022-10-31 15:53:43 -0400

    sidekiq is not used

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -4,7 +4,6 @@ if Settings.datadog.enabled
   require 'ddtrace'
   Datadog.configure do |c|
     c.use :rails
-    c.use :sidekiq, analytics_enabled: true
     c.use :redis
     c.tracer env: Settings.datadog.env || Rails.env
   end
