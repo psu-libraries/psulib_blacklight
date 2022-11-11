df81943255458dbc811f61f91f822f2fa6a99f5d
Alex Kiessling <32677188+ajkiessl@users.noreply.github.com>
2022-11-11 15:29:13 -0500

    Configuration for custom error pages. Added some tests to test the error page renders when an exception is risen.  Routing test too. (#1078)

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,8 @@ module Psulcat
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Use our own custom exceptions
+    config.exceptions_app = routes
   end
 end
--- spec/routing/routing_spec.rb
+++ spec/routing/routing_spec.rb
@@ -2,8 +2,12 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Page not found', type: :routing do
+RSpec.describe 'Error Routing', type: :routing do
   it 'routes to page not found if path is completely invalid' do
     expect(get: 'kaljhgl').to route_to controller: 'errors', action: 'not_found', catch_unknown_routes: 'kaljhgl'
+  end
+
+  it 'routes to internal server error if directed to the 500 path' do
+    expect(get: '/500').to route_to controller: 'errors', action: 'internal_server_error'
   end
 end
