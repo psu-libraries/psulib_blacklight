7865fc2e53b74e9a1f19c0670dbb89b1ad17ebf6
Alex Kiessling <ajkiessl@gmail.com>
2022-11-21 12:48:31 -0500

    Removed some stuff that isn't needed

--- app/components/external_links/base_links_component.rb
+++ app/components/external_links/base_links_component.rb
@@ -3,7 +3,7 @@
 module ExternalLinks
   class BaseLinksComponent < ViewComponent::Base
     def initialize(links:, heading: nil, show_links: true)
-      @links = sorted_links(links)
+      @links = links
       @heading = heading
       @show_links = show_links
       @data_target = data_target
@@ -29,31 +29,9 @@ module ExternalLinks
       links.slice(2, links.length - 2)
     end
 
-    def special_collections_materials_only?
-      links.map { |l| l['url'].include?('ark:/42409/fa8') }.count(true) == links.count
-    end
-
-    def card_class
-      special_collections_materials_only? ? 'bs-callout bs-callout-psu' : 'bs-callout bs-callout-primary'
-    end
-
-    def heading_display
-      special_collections_materials_only? ? 'Special Collections Materials' : heading
-    end
-
-    def list_class
-      special_collections_materials_only? ? 'external-links-psu-digital-collections' : 'external-links'
-    end
-
     private
 
       attr_reader :links, :heading
-
-      def sorted_links(links_arg)
-        return links_arg unless links_arg.is_a?(Array)
-
-        links_arg.sort_by { |l| l['url'].include?('ark:/42409/fa8') ? 0 : 1 }
-      end
 
       def show_links?
         !!@show_links
