52f31d6e65ffc93832efa02490d9129c5fc5c246
whereismyjetpack <djb44@psu.edu>
2022-12-06 10:11:50 -0500

    set delivery_method

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.smtp_settings = {
     address: ENV.fetch('SMTP_ADDRESS', 'localhost'),
     port: ENV.fetch('SMTP_PORT', 25),
