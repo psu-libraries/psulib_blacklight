856bb3d4c96cc296c8b1b317aaa4ea4f7152a570
Alex Kiessling <ajkiessl@gmail.com>
2023-04-17 16:30:41 -0400

    Remove whitespace

--- spec/features/campus_facet_all_online_spec.rb
+++ spec/features/campus_facet_all_online_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe 'Campus Facet Add All Online', type: :feature do
   describe 'Adding and removing online items to campus facet filter', js: true do
     it 'adds online items' do
-      # This test is dependent on there being 21 Behrend items and 134 
+      # This test is dependent on there being 21 Behrend items and 134
       # Online items. Adding records to the fixtures could break this.
       visit '/?f[campus_facet][]=Penn+State+Behrend'
       expect(page).to have_content ' 1 - 10 of 21 '
