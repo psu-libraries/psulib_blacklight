bc7715c4d20108c89cab70fcd59a9aab3ac43682
Smulligan <smm5878@psu.edu>
2023-04-27 08:50:52 -0400

    refactors with new holdings service class

--- app/controllers/availability/availability_controller.rb
+++ app/controllers/availability/availability_controller.rb
@@ -7,23 +7,13 @@ module Availability
     def sirsi_data
       args = "&includeBoundTogether=true&titleID=#{params[:title_ids].join('&titleID=')}"
 
-      make_sirsi_request(args)
+      render xml: HoldingsRequestService.new(args).make_holdings_request
     end
 
     def sirsi_item_data
       args = "&itemID=#{params[:item_ids].join('&itemID=')}"
 
-      make_sirsi_request(args)
+      render xml: HoldingsRequestService.new(args).make_holdings_request
     end
-
-    private
-
-      def make_sirsi_request(args)
-        sirsi_uri = URI(Settings.sirsi_url + args)
-
-        sirsi_response = Net::HTTP.get(sirsi_uri)
-
-        render xml: sirsi_response
-      end
   end
 end
