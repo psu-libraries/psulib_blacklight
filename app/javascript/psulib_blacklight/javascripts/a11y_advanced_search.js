a55fd70c7867b53c9acec6724a462d546f161b0d
Alex Kiessling <32677188+ajkiessl@users.noreply.github.com>
2023-04-28 15:50:09 -0400

    Adds javascript to add some aria labels to dropdowns in advanced search (#1172)
    * Adds javascript to add some aria labels to dropdowns in advanced search
    
    * Niftany
    
    * Some minor and micro updates that may or may not fix the screen reader multiselect dropdown issues
    
    * Adds aria label and aria multiselectable to the multi select facet dropdowns on the advanced search page
    
    * Lint
    
    * Comment
    
    * Fixed aria labels

---
+++ app/javascript/psulib_blacklight/javascripts/a11y_advanced_search.js
@@ -0,0 +1,23 @@
+$(document).ready(() => {
+  const advancedSearchPage = document.getElementsByClassName(
+    'advanced-search-form'
+  );
+  // Check to see if we are in the advanced search page
+  if (advancedSearchPage.length > 0) {
+    // Set ARIA attribute for "op" dropdown
+    const opSelect = document.getElementById('op');
+    opSelect.setAttribute('aria-haspopup', 'listbox');
+    // Set ARIA attribute for "sort" dropdown
+    const sortSelect = document.getElementById('sort');
+    sortSelect.setAttribute('role', 'listbox');
+    // Set ARIA label and multiselect for multiselect drop downs
+    const multiDropDowns = document.querySelectorAll('[multiple="multiple"]');
+    for (let i = 0; i < multiDropDowns.length; i += 1) {
+      const label = multiDropDowns[i].parentElement.parentElement
+        .querySelectorAll('label')[0]
+        .textContent.trim();
+      multiDropDowns[i].setAttribute('aria-label', label);
+      multiDropDowns[i].setAttribute('aria-multiselectable', 'true');
+    }
+  }
+});
