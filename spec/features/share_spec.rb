303c8067294cf5edec18c84424b2378157251ad9
Smulligan <smm5878@psu.edu>
2023-05-08 12:09:44 -0400

    renamed and refactored to include RIS export in test

--- spec/features/share_spec.rb
+++ spec/features/share_spec.rb
@@ -2,7 +2,10 @@
 
 require 'rails_helper'
 
-RSpec.describe "Using the email feature in the 'Share' dropdown", type: :feature do
+RSpec.describe "Using the 'Share' dropdown", type: :feature do
+  let(:expected_content_type) { 'application/x-research-info-systems' }
+  let(:expected_file_name) { 'document.ris' }
+
   it 'renders email prompt and sends email' do
     visit '/catalog/22090269'
     click_on 'Share'
@@ -18,5 +21,15 @@ RSpec.describe "Using the email feature in the 'Share' dropdown", type: :feature
     expect(ActionMailer::Base.deliveries.first.body.to_s).to include 'Message: Message'
     expect(ActionMailer::Base.deliveries.first.body.to_s).to include 'http://www.example.com/catalog/22090269'
     expect(ActionMailer::Base.deliveries.first.subject).to include 'PSU Libraries Item Record: Ethical and Social'
+  end
+
+  it 'renders RIS file in the dropdown and downloads an RIS of the record' do
+    visit '/catalog/22090269'
+    click_on 'Share'
+    expect(page).to have_link 'RIS file'
+
+    click_on 'RIS file'
+    expect(page.response_headers['Content-Disposition']).to include("attachment; filename=\"#{expected_file_name}\"")
+    expect(page.response_headers['Content-Type']).to eq(expected_content_type)
   end
 end
