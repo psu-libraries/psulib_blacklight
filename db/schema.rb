25b3bbf756226e969328974e161a26f9812906bd
Dann <whereismyjetpack@users.noreply.github.com>
2023-05-12 10:11:18 -0400

    preview/db searches (#1183)
    * Revert "Use Redis to store searches (#730)"
    
    This reverts commit bd7d2381118c328577fa32f09f285f3a8e284f62.
    
    * re-add searchs via migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_02_203924) do
+ActiveRecord::Schema.define(version: 2023_05_11_165922) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -50,6 +50,15 @@ ActiveRecord::Schema.define(version: 2022_08_02_203924) do
     t.datetime "updated_at", null: false
     t.index ["document_id"], name: "index_bookmarks_on_document_id"
     t.index ["user_id"], name: "index_bookmarks_on_user_id"
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.binary "query_params"
+    t.integer "user_id"
+    t.string "user_type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_searches_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
