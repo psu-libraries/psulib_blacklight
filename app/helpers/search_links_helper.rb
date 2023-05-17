62e309d4be1e404dcf6d7fbe688cee3711ebf1bb
Alex Kiessling <ajkiessl@gmail.com>
2023-05-17 13:48:50 -0400

    Niftany

--- app/helpers/search_links_helper.rb
+++ app/helpers/search_links_helper.rb
@@ -28,31 +28,31 @@ module SearchLinksHelper
 
   private
 
-  def generate_links(link_type, options)
-    result = []
-    options[:value].zip(strict_titles(link_type, options)).each do |item, zipped|
-      lnk = link_to(item,
-                    "/?#{search_type(link_type)}=#{CGI.escape(zipped || item)}",
-                    class: title_search_html_class(link_type), 
-                    title: search_field?(link_type) && link_type != :title ? "Search: #{zipped || item}" : nil)
-      result << content_tag('li', lnk, nil, false)
+    def generate_links(search_type, options)
+      result = []
+      options[:value].zip(strict_titles(search_type, options)).each do |item, zipped|
+        lnk = link_to(item,
+                      "/?#{search_query(search_type)}=#{CGI.escape(zipped || item)}",
+                      class: title_search_html_class(search_type),
+                      title: search_field?(search_type) && search_type != :title ? "Search: #{zipped || item}" : nil)
+        result << content_tag('li', lnk, nil, false)
+      end
+      content_tag 'ul', result.join, nil, false
     end
-    content_tag 'ul', result.join, nil, false
-  end
 
-  def search_field?(link_type)
-    [:series, :title, :subject].include?(link_type)
-  end
+    def search_field?(search_type)
+      [:series, :title, :subject].include?(search_type)
+    end
 
-  def search_type(link_type)
-    search_field?(link_type) ? "search_field=#{link_type.to_s}&q" : "f[#{link_type.to_s.concat("_facet")}][]"
-  end
+    def search_query(search_type)
+      search_field?(search_type) ? "search_field=#{search_type}&q" : "f[#{search_type.to_s.concat('_facet')}][]"
+    end
 
-  def title_search_html_class(link_type)
-    search_field?(link_type) && link_type != :title ? "search-#{link_type.to_s}" : nil
-  end
+    def title_search_html_class(search_type)
+      search_field?(search_type) && search_type != :title ? "search-#{search_type}" : nil
+    end
 
-  def strict_titles(link_type, options)
-    link_type == :series ? options[:document][:series_title_strict_tsim] || [] : []
-  end
+    def strict_titles(search_type, options)
+      search_type == :series ? options[:document][:series_title_strict_tsim] || [] : []
+    end
 end
