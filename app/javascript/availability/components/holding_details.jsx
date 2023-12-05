da20822180a7862a31f7e3f3f7ab8e6f2dd6926f
Smulligan <smm5878@psu.edu>
2023-12-05 10:36:06 -0500

    lint fix

--- app/javascript/availability/components/holding_details.jsx
+++ app/javascript/availability/components/holding_details.jsx
@@ -4,22 +4,20 @@ import LocationInfo from './location_info';
 import CourseReserveDueDate from './course_reserve_due_date';
 import MapScanLink from './map_scan_link';
 
-const HoldingDetails = ({ holding }) => {
-  return (
-    <>
-      <td>
-        {holding.callNumber}
-        <PublicNote holding={holding} />
-      </td>
-      <td>{holding.itemType}</td>
-      <td>
-        <LocationInfo holding={holding} />
-        <CourseReserveDueDate holding={holding} />
-        <MapScanLink holding={holding} />
-      </td>
-    </>
-  );
-};
+const HoldingDetails = ({ holding }) => (
+  <>
+    <td>
+      {holding.callNumber}
+      <PublicNote holding={holding} />
+    </td>
+    <td>{holding.itemType}</td>
+    <td>
+      <LocationInfo holding={holding} />
+      <CourseReserveDueDate holding={holding} />
+      <MapScanLink holding={holding} />
+    </td>
+  </>
+);
 
 // eslint-react: defines valid prop types passed to this component
 HoldingDetails.propTypes = {
