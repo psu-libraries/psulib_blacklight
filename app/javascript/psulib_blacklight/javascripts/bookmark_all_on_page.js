32b31aca6c8b2e45532b2104c2e2abf7a2be8b7e
Alex Kiessling <ajkiessl@gmail.com>
2023-12-15 13:54:20 -0500

    Linting

--- app/javascript/psulib_blacklight/javascripts/bookmark_all_on_page.js
+++ app/javascript/psulib_blacklight/javascripts/bookmark_all_on_page.js
@@ -1,9 +1,9 @@
 import CheckboxSubmitAll from './checkbox_submit_all';
 
 $(document).ready(() => {
-  let bookmarkAllButton = $('#bookmark-all');
-  bookmarkAllButton.on("click", function () {
-    let bookmarkCheckboxes = $('form[data-absent="Bookmark"]');
+  const bookmarkAllButton = $('#bookmark-all');
+  bookmarkAllButton.on('click', () => {
+    const bookmarkCheckboxes = $('form[data-absent="Bookmark"]');
 
     new CheckboxSubmitAll(bookmarkCheckboxes).clicked();
   });
