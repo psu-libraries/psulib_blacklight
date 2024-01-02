49bcb38ad293ae4afcd90297314aa028bc285043
Alex Kiessling <ajkiessl@gmail.com>
2024-01-02 13:11:15 -0500

    Removes old bookmark test

--- spec/features/bookmarks_spec.rb
+++ spec/features/bookmarks_spec.rb
@@ -27,25 +27,6 @@ RSpec.describe 'Bookmarks', type: :feature do
       login_as(user, scope: :user)
     end
 
-    context 'when managing a single Bookmark' do
-      it 'Adds, removes, re-adds, and views a bookmarked record', js: true do
-        visit '/?utf8=✓&search_field=all_fields&q=Ethical+and+Social+Issues+in+the+Information+Age+AND+9783319707129'
-        bookmark_buttons = find_all('.toggle-bookmark')
-        bookmark_buttons.first.click
-        sleep 0.5
-        expect(page).to have_content 'In Bookmarks'
-        bookmark_buttons.first.click
-        sleep 0.5
-        expect(page).not_to have_content 'In Bookmarks'
-        bookmark_buttons.first.click
-        sleep 0.5
-        click_link 'Bookmarks'
-        expect(page).to have_css :h1, text: 'Bookmarks'
-        expect(page).to have_content '1 entry found'
-        expect(page).to have_content 'Ethical and Social Issues in the Information Age'
-      end
-    end
-
     context 'when Bookmarking all on page' do
       it 'Adds, removes, re-adds bookmarks, and views Bookmarks page', js: true do
         visit '/'
@@ -84,6 +65,7 @@ RSpec.describe 'Bookmarks', type: :feature do
         end
         # Test that the Bookmarks page has proper content
         click_link 'Bookmarks'
+        expect(page).to have_css :h1, text: 'Bookmarks'
         expect(page).to have_content '1 - 10 of 10'
         expect(page).to have_content first_title
       end
