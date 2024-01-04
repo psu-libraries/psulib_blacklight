1fe926b8eeb21cf453d81c6000f95b483a2f9b2b
Alex Kiessling <ajkiessl@gmail.com>
2024-01-04 10:59:55 -0500

    Functionality to get rtl titles aligned properly in index, show and browse pages done

--- app/javascript/psulib_blacklight/javascripts/show_default.js
+++ app/javascript/psulib_blacklight/javascripts/show_default.js
@@ -8,4 +8,13 @@ $(document).ready(() => {
       $(this).prevAll().removeClass('field-hierarchy');
     }
   );
+
+  // Align rtl text properly
+  $('article').each(function () {
+    const titleLink = this.querySelector('.index_title').querySelector('a');
+    titleLink.setAttribute('dir', 'auto')
+    if (getComputedStyle(titleLink).direction === "rtl") {
+      titleLink.setAttribute('class', 'float-right text-align-start col-sm-11 p-0 pr-4')
+    }
+  })
 });
