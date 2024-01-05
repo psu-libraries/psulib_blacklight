d2fc2d515573a2cf7e5e94248f3f3144550dd620
Alex Kiessling <32677188+ajkiessl@users.noreply.github.com>
2024-01-05 15:48:40 -0500

    Right-align rtl text in title fields (#1261)
    * Functionality to get rtl titles aligned properly in index, show and browse pages done
    
    * Lint
    
    * Export the show_default methods and import where needed.  Adds jest test to test the alignRtl method
    
    * moved some things around to be better organized
    
    * Lint
    
    * Reverted to == since we aren't doing a strict match

--- app/javascript/psulib_blacklight/javascripts/index.js
+++ app/javascript/psulib_blacklight/javascripts/index.js
@@ -1,7 +1,10 @@
 import './show_default';
+import alignRtl from './align_rtl_index';
 import './google_preview';
 import './a11y_advanced_search';
 import './bookmark_all_on_page';
+
+$(document).ready(alignRtl());
 
 // Otherwise bootstrap-select won't fire on turbolinked clicks to Advance Search
 $(document).on('turbolinks:load', () => {
