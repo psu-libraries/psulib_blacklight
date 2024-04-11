837144c6c543a3b3aaba464d4f8ef140c643b625
Alex Kiessling <ajkiessl@gmail.com>
2024-04-11 17:07:14 -0400

    Use let instead of var with loop.  Display fix

--- app/javascript/psulib_blacklight/javascripts/hathilink.js
+++ app/javascript/psulib_blacklight/javascripts/hathilink.js
@@ -2,10 +2,10 @@ function hathilink() {
   const elements = document.getElementsByClassName("hathilink");
   const googlePreviewElement = document.getElementById('google-preview');
 
-  for (var n = 0; n < elements.length; n++) {
+  for (let i = 0; i < elements.length; i++) {
     const urlComponents = {
       hathitrustUrl: 'https://catalog.hathitrust.org/api/volumes/brief/',
-      searchItem: elements[n].dataset.searchItem,
+      searchItem: elements[i].dataset.searchItem,
     };
     const { hathitrustUrl, searchItem } = urlComponents;
     const hathiLinkUrlQuery = `${hathitrustUrl}${searchItem}.json`;
@@ -16,17 +16,17 @@ function hathilink() {
         success(response) {
           const bookInfo = response["items"];
           if (bookInfo) {
-            setLink(bookInfo);
+            setLink(bookInfo, elements[i]);
           }
         },
       });
     }
 
-    let setLink = function (bookInfo) {
-      for (var i = 0; i < bookInfo.length; i++) {
+    let setLink = function (bookInfo, element) {
+      for (let i = 0; i < bookInfo.length; i++) {
         if (bookInfo[i].usRightsString === "Full view") {
-          elements[n].firstElementChild.href = bookInfo[i].itemURL;
-          elements[n].style.display = '';
+          element.firstElementChild.href = bookInfo[i].itemURL;
+          element.style.display = '';
           // return to break the loop and return true so we know we don't need a google preview
           return true
         }
