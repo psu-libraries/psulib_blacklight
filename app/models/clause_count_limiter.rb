2487a3bf23ba162c81e798ba8a3161b3f3d557f5
Jesse Landis-Eigsti <jml8735@psu.edu>
2024-05-01 10:13:19 -0600

    1053 - Makes formatting changes requested by Rubocop

--- app/models/clause_count_limiter.rb
+++ app/models/clause_count_limiter.rb
@@ -1,23 +1,21 @@
+# frozen_string_literal: true
+
 module ClauseCountLimiter
   # Limits the number of clauses allowed in a search field. Varies by search field type.
   def limit_clause_count(solr_parameters)
-    return unless is_query_present?(solr_parameters)
+    return unless query_present?(solr_parameters)
 
     length = case blacklight_params['search_field']
              when 'all_fields'
                # 10 terms, 1 will be added while truncating
                10
-             when 'title'
-               # 27 terms + "edismax", "qf", "title_qf", "pf", "title_pf", "pf3", "title_pf3", "pf2", "title_pf2"
+             when 'title', 'author'
+               # 27 terms + "edismax", "qf", "title/author_qf", "pf", "title/author_pf", "pf3", "title/author_pf3",
+               # "pf2", "title/author_pf2"
                36
-             when 'author'
-               # 27 terms + "edismax", "qf", "author_qf", "pf", "author_pf", "pf3", "author_pf3", "pf2", "author_pf2"
-               36
-             when 'subject'
+             when 'subject', 'isbn_issn'
                # 6 + "edismax",  "qf", "subject_qf", "pf", "subject_pf", "pf3", "subject_pf3", "pf2", "subject_pf2"
-               15
-             when 'isbn_issn'
-               # 9 + "edismax", "qf", "isbn_issn_qf", "pf", "pf3", "pf2"
+               # or # 9 + "edismax", "qf", "isbn_issn_qf", "pf", "pf3", "pf2"
                15
              else
                # 20 fallback for any other search field such as publisher, series_statement,
@@ -30,12 +28,12 @@ module ClauseCountLimiter
 
   private
 
-    def is_query_present?(solr_parameters)
+    def query_present?(solr_parameters)
       solr_parameters[:q].present? && blacklight_params[:q].present?
     end
 
     def get_query_length(solr_parameters)
-      solr_parameters[:q].split(/\b/).select { |x| x.match?(/\w/) }.length
+      solr_parameters[:q].split(/\b/).grep(/\w/).length
     end
 
     def truncate_query(solr_parameters, length)
