e1a6ddb3152cd5fd52af8aa16f70e7aa52d82ada
Smulligan <smm5878@psu.edu>
2024-05-09 15:00:30 -0400

    previous_next_doc partial is depreciated, replaced with component. This modifies how session_tracking_params is called as well

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -9,17 +9,17 @@ module UrlHelper
   # @example
   #   session_tracking_params(SolrDocument.new(id: 123), 7)
   #   => { data: { :'context-href' => '/catalog/123/track?counter=7&search_id=999' } }
-  def session_tracking_params(document, counter)
-    path = session_tracking_path(document,
-                                 per_page: params.fetch(:per_page, search_session['per_page']),
-                                 counter: counter,
-                                 search_id: current_search_session.try(:id),
-                                 document_id: document&.id)
 
-    if path.nil?
-      return {}
+  def session_tracking_params document, counter, per_page: search_session['per_page'], search_id: current_search_session&.id
+    path_params = { per_page: params.fetch(:per_page, per_page), counter: counter, search_id: search_id }
+    if blacklight_config.track_search_session.storage == 'server'
+      path_params[:document_id] = document&.id
+      path_params[:search_id] = search_id
     end
+    path = session_tracking_path(document, path_params)
+    return {} if path.nil?
 
-    { data: { 'context-href': path } }
+    context_method = blacklight_config.track_search_session.storage == 'client' ? 'get' : 'post'
+    { data: { context_href: path, context_method: context_method, turbo_prefetch: false } }
   end
 end
