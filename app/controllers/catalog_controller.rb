36b31405534144243267835ea6e1e027280acb4e
Dann Bohn <dannbohn@gmail.com>
2024-07-25 16:00:06 -0400

    turns on crawler_detector

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -398,6 +398,9 @@ class CatalogController < ApplicationController
 
     config.add_search_field 'all_fields', label: 'Keyword'
 
+    # Add Crawler Detector. If the session is a crawler we will not save the search
+    config.crawler_detector  = lambda { |req| req.env['HTTP_USER_AGENT'] =~ /bot/ }
+
     # Now we see how to over-ride Solr request handler defaults, in this
     # case for a BL "search field", which is really a dismax aggregate
     # of Solr search fields.
