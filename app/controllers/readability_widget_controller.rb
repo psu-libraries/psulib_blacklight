09b759fde40c6de12e01c263ae4027cf58dc13d4
Smulligan <smm5878@psu.edu>
2024-07-29 14:41:40 -0400

    niftany

--- app/controllers/readability_widget_controller.rb
+++ app/controllers/readability_widget_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class ReadabilityWidgetController < ApplicationController
-    def widget
-        render layout: false
-    end  
+  def widget
+    render layout: false
+  end
 end
