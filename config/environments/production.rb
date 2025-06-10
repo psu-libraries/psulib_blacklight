7865313be6a247e31fc844c9897a76a6f6c8fecf
Alex Kiessling <ajkiessl@gmail.com>
2025-06-10 16:02:53 -0400

    remove deprecation silencer file and replaced in production env config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
-  config.active_support.deprecation = :notify
+  config.active_support.deprecation = :silience
 
   # Log disallowed deprecations.
   config.active_support.disallowed_deprecation = :log
--- config/initializers/deprecation_silencers.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Silence deprecation warnings made in the deprecation gem and in active-support
-
-if ENV['RAILS_ENV'] == 'production'
-  Deprecation.default_deprecation_behavior = :silence
-  ActiveSupport::Deprecation.behavior = [:silence]
-end
