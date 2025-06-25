f02b637c8369f226d3a319446c6317c132ff2e5b
granta03 <grantallendx@yahoo.com>
2025-06-25 13:39:09 -0400

    styling fixes

--- app/javascript/availability/components/availability.jsx
+++ app/javascript/availability/components/availability.jsx
@@ -67,7 +67,7 @@ const Availability = ({ structuredHoldings, summaryHoldings }) => (
             Listing where to find this item in the library.
           </h5>
           <table id={`holdings-${uniqueID}`} className="table table-sm">
-            <thead className="table-secondary">
+            <thead className="table-secondary border-top">
               <tr>
                 <th>Call number</th>
                 <th>Material</th>
