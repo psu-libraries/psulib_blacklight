23fab92890e4e7080e54e0ce16e303f1efd343bb
Alex Kiessling <32677188+ajkiessl@users.noreply.github.com>
2025-07-11 10:27:06 -0400

    Fix for report issue form (#1456)
    * Using 'disabled: true' prevented the data from being passed along in the report issue form.  Changed this to 'read_only: true' and muted the fields visually.  Added some tests too.
    
    * Remove extra :js
    
    * Adds sleeps to wait for modal to appear
    
    * Not sure why these are failing in CI.  They work fine locally.  Moving the :js around
    
    * Adds sleep after page load

---
+++ spec/features/report_issue_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Report Issue Form' do
+  describe 'when a comment is included' do
+    it 'submits the report issue form successfully and sends an email', :js do
+      visit '/catalog/19437'
+      sleep 0.5
+      click_on 'Report an Issue'
+
+      fill_in 'comment', with: 'There is an issue with this record.'
+      fill_in 'email', with: 'user@example.com'
+      click_button I18n.t('blacklight.sms.form.submit')
+
+      expect(page).to have_content(I18n.t('blacklight.email.success'))
+      mail = ActionMailer::Base.deliveries.last
+      expect(mail).to be_present
+      expect(mail.subject).to include('Penn State University Libraries Catalog - Issue Report')
+      expect(mail.body.encoded).to include('There is an issue with this record.')
+      expect(mail.body.encoded).to include('user@example.com')
+      expect(mail.body.encoded).to include('/catalog/19437')
+      expect(mail.body.encoded).to include('Title: The theory of wages')
+    end
+  end
+
+  describe 'when a comment is not included' do
+    it 'does not submit form or send an email', :js do
+      visit '/catalog/19437'
+      sleep 0.5
+      click_on 'Report an Issue'
+
+      fill_in 'comment', with: ''
+      fill_in 'email', with: 'user@example.com'
+      click_button I18n.t('blacklight.sms.form.submit')
+
+      expect(ActionMailer::Base.deliveries).to be_empty
+    end
+  end
+end
