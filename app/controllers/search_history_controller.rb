6f4a00f957336319daea0ccf01bc9a0e631f4fd0
granta03 <grantallendx@yahoo.com>
2025-11-03 08:21:16 -0500

    Removed RangeLimitHelper from failing file

--- app/controllers/search_history_controller.rb
+++ app/controllers/search_history_controller.rb
@@ -3,7 +3,4 @@
 class SearchHistoryController < ApplicationController
   include Blacklight::SearchHistory
 
-  # TODO: Consider deleting the following line altogether. The following line was generated during an install.
-  # Removing the line has zero effect on the display of constraints.
-  helper RangeLimitHelper
 end
