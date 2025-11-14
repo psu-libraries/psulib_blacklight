3df546478ea2e246d49fe007a9fab5ecb42a4b9a
Alex Kiessling <ajkiessl@gmail.com>
2025-11-14 10:33:29 -0500

    Adds configuration to bugsnag to ignore '/health' paths

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -3,4 +3,12 @@
 Bugsnag.configure do |config|
   config.app_version = ENV.fetch('APP_VERSION', nil)
   config.release_stage = ENV.fetch('BUGSNAG_RELEASE_STAGE', 'development')
+
+  config.add_on_error do |event|
+    path = event.request[:path] rescue nil
+
+    if path&.start_with?("/health")
+      event.ignore!
+    end
+  end
 end
