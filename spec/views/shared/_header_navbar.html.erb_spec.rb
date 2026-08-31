5ae516b9599612f242e82adc79ff4a8b3cc9a8cb
Jesse Landis-Eigsti <jml8735@psu.edu>
2026-08-31 09:08:21 -0600

    WIP – some tests fixed
    Addresses issues with header_navbar tests, bookmark and share tests,
    and all those that were rendering the above components. Share menu still
    starts opened rather than opening on click.

--- spec/views/shared/_header_navbar.html.erb_spec.rb
+++ spec/views/shared/_header_navbar.html.erb_spec.rb
@@ -8,7 +8,8 @@ RSpec.describe 'shared/_header_navbar' do
   before do
     stub_template 'shared/_user_util_links.html.erb' => 'Some content'
     stub_template 'catalog/_search_form.html.erb' => 'Other content'
-    assign(:search_bar, Blacklight::SearchBarPresenter.new(controller, blacklight_config))
+    # "The SearchBarPresenter has been deprecated. Call `render Blacklight::SearchBarComponent.new' instead"
+    # assign(:search_bar, Blacklight::SearchBarPresenter.new(controller, blacklight_config))
   end
 
   it 'displays search bar except on Advanced Search' do
