1bb64ac755e5ced7649766bf8f0e691c58828038
Smulligan <smm5878@psu.edu>
2026-09-09 16:47:03 -0400

    restore RIS link name

--- spec/features/share_spec.rb
+++ spec/features/share_spec.rb
@@ -27,6 +27,6 @@ RSpec.describe "Using the 'Share' dropdown" do
     visit '/catalog/22090269'
     sleep 0.5
     click_on 'Share'
-    expect(page).to have_link 'RIS'
+    expect(page).to have_link 'RIS File'
   end
 end
