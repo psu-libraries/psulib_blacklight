f29f37585d648244a3d1755248ed808a8aeaf434
Jesse Landis-Eigsti <jml8735@psu.edu>
2026-09-22 08:05:27 -0600

    Removes duplicate blacklight import

--- app/javascript/psulib_blacklight/styles/psulib_base_overrides/index.js
+++ app/javascript/psulib_blacklight/styles/psulib_base_overrides/index.js
@@ -1,13 +1,1 @@
-// Vendor
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-
-// Blacklight Upstream Javascript
-// Picking and choosing rather than taking all JS from BL
-import 'blacklight-frontend/app/javascript/blacklight/core';
-import 'blacklight-frontend/app/javascript/blacklight/checkbox_submit';
-import 'blacklight-frontend/app/javascript/blacklight/modal';
-import 'blacklight-frontend/app/javascript/blacklight/button_focus';
-import 'blacklight-frontend/app/javascript/blacklight/facet_load';
-import 'blacklight-frontend/app/javascript/blacklight/search_context';
-
 import './overrides.scss';
