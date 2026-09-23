ca2ff2b9252ed2daf34f7d1b4f392b30de3bfd11
Alex Kiessling <32677188+ajkiessl@users.noreply.github.com>
2026-09-23 15:37:23 -0400

    Do not display Hathitrust or Google preview links if content is already "Online" (#1619)
    * Extends logic in shared free_to_read? function to return true if the access facet includes Online.  Adds tests for both the google preview link and hathi link to check that the links don't display if Online
    
    * Refactors tests

--- spec/components/external_links/hathi_link_component.html.erb_spec.rb
+++ spec/components/external_links/hathi_link_component.html.erb_spec.rb
@@ -47,13 +47,27 @@ RSpec.describe ExternalLinks::HathiLinkComponent, type: :component do
     end
   end
 
-  context 'when document has an ISSN, but is Free to Read' do
-    let(:document) { { 'access_facet' => ['Free to Read', 'In the Library'], 'issn_ssm' => ['1234-1234'] } }
+  context 'when document has an ISSN' do
+    let(:document) { { 'access_facet' => access_facet, 'issn_ssm' => ['1234-1234'] } }
 
-    it 'renders a hidden link with no attached search term data' do
-      expect(rendered).to have_css("img[src*='Springshare-LibGuide-Image-100X100-872acafb7418ee18a303.png']",
-                                   visible: :hidden)
-        .and have_css("div[data-search-item='']", visible: :hidden)
+    context 'when it is Free to Read' do
+      let(:access_facet) { ['Free to Read', 'In the Library'] }
+
+      it 'renders a hidden link with no attached search term data' do
+        expect(rendered).to have_css("img[src*='Springshare-LibGuide-Image-100X100-872acafb7418ee18a303.png']",
+                                     visible: :hidden)
+          .and have_css("div[data-search-item='']", visible: :hidden)
+      end
+    end
+
+    context 'when it is Online' do
+      let(:access_facet) { ['Online', 'In the Library'] }
+
+      it 'renders a hidden link with no attached search term data' do
+        expect(rendered).to have_css("img[src*='Springshare-LibGuide-Image-100X100-872acafb7418ee18a303.png']",
+                                     visible: :hidden)
+          .and have_css("div[data-search-item='']", visible: :hidden)
+      end
     end
   end
 end
